4035b71df4d2d8ed0ab774a4969f99b61b7757a1
streamhsa <sandeep.kumar@amd.com>
2016-02-10 20:01:16 +0800

    Resolved test issues

--- tests/src/hip_ballot.cpp
+++ tests/src/hip_ballot.cpp
@@ -15,16 +15,19 @@ __global__ void
 
 
 int main(int argc, char *argv[])
-{
-
+{ int warpSize;
+  hipDeviceProp_t devProp;
+  hipDeviceGetProperties(&devProp, 0);
+  if(strncmp(devProp.name,"Fiji",1)==0)  warpSize =64;
+  else warpSize =32;
   unsigned int Num_Threads_per_Block      = 512;
   unsigned int Num_Blocks_per_Grid        = 1;
-  unsigned int Num_Warps_per_Block        = Num_Threads_per_Block/64;
-  unsigned int Num_Warps_per_Grid         = (Num_Threads_per_Block*Num_Blocks_per_Grid)/64;
+  unsigned int Num_Warps_per_Block        = Num_Threads_per_Block/warpSize;
+  unsigned int Num_Warps_per_Grid         = (Num_Threads_per_Block*Num_Blocks_per_Grid)/warpSize;
   unsigned int* host_ballot = (unsigned int*)malloc(Num_Warps_per_Grid*sizeof(unsigned int));
   unsigned int* device_ballot; 
   HIP_ASSERT(hipMalloc((void**)&device_ballot, Num_Warps_per_Grid*sizeof(unsigned int)));
-
+  int divergent_count =0;
   for (int i=0; i<Num_Warps_per_Grid; i++) host_ballot[i] = 0;
 
  
@@ -36,13 +39,13 @@ int main(int argc, char *argv[])
   HIP_ASSERT(hipMemcpy(host_ballot, device_ballot, Num_Warps_per_Grid*sizeof(unsigned int), hipMemcpyDeviceToHost));
   for (int i=0; i<Num_Warps_per_Grid; i++) {
 
-     if ((host_ballot[i] == 0)||(host_ballot[i]/64 == 64)) std::cout << "Warp " << i << " IS convergent- Predicate true for " << host_ballot[i]/64 << " threads\n";
+     if ((host_ballot[i] == 0)||(host_ballot[i]/warpSize == warpSize)) std::cout << "Warp " << i << " IS convergent- Predicate true for " << host_ballot[i]/warpSize << " threads\n";
 
-     else std::cout << "Warp " << i << " IS divergent - Predicate true for " << host_ballot[i]/64<< " threads\n";
-
+     else {std::cout << "Warp " << i << " IS divergent - Predicate true for " << host_ballot[i]/warpSize<< " threads\n";
+	  divergent_count++;}
 }
 
-
+if (divergent_count==1) printf("PASSED"); else printf("FAILED");
   return EXIT_SUCCESS;
 
 }
