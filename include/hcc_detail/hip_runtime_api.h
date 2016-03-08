a094cdba64be20644d0c94f09aa1f7d5ab823c55
Maneesh Gupta <maneesh.gupta@amd.com>
2016-03-08 11:14:03 +0530

    Fix release notes and minimum required hcc workweek check

--- include/hcc_detail/hip_runtime_api.h
+++ include/hcc_detail/hip_runtime_api.h
@@ -33,7 +33,7 @@ THE SOFTWARE.
 #include <hip_runtime_api.h>
 #include "hip_hcc.h"
 
-#if defined (__HCC__) &&  (__hcc_workweek__ < 1602)
+#if defined (__HCC__) &&  (__hcc_workweek__ < 16074)
 #error("This version of HIP requires a newer version of HCC.");
 #endif
 
