e4081567ae35c225fa9d3b814d7da43db99495eb
dfukalov <daniil.fukalov@amd.com>
2016-03-26 16:01:49 +0300

    removed FileCheck dependency & significantly improved test coverage
    [ROCm/hip commit: 28d29cac822a9abfd31b4475be03360d0a3d8a8c]

--- test/axpy.cu
+++ test/axpy.cu
@@ -1,10 +1,9 @@
 // RUN: hipify "%s" -o=%t --
-// RUN: FileCheck %s -input-file=%t --match-full-lines
 
 #include <iostream>
 
 __global__ void axpy(float a, float* x, float* y) {
-  // CHECK: y[hipThreadIdx_x] = a * x[hipThreadIdx_x];
+  // RUN: sh -c "test `grep -c -F 'y[hipThreadIdx_x] = a * x[hipThreadIdx_x];' %t` -eq 2"
   y[threadIdx.x] = a * x[threadIdx.x];
 }
 
@@ -18,16 +17,21 @@ int main(int argc, char* argv[]) {
   // Copy input data to device.
   float* device_x;
   float* device_y;
+  // RUN: sh -c "test `grep -c -F 'hipMalloc(&device_x, kDataLen * sizeof(float));' %t` -eq 2"
   cudaMalloc(&device_x, kDataLen * sizeof(float));
+  // RUN: sh -c "test `grep -c -F 'hipMalloc(&device_y, kDataLen * sizeof(float));' %t` -eq 2"
   cudaMalloc(&device_y, kDataLen * sizeof(float));
+  // RUN: sh -c "test `grep -c -F 'hipMemcpy(device_x, host_x, kDataLen * sizeof(float), hipMemcpyHostToDevice);' %t` -eq 2"
   cudaMemcpy(device_x, host_x, kDataLen * sizeof(float), cudaMemcpyHostToDevice);
 
   // Launch the kernel.
-  // CHECK: hipLaunchKernel(HIP_KERNEL_NAME(axpy), dim3(1), dim3(kDataLen), 0, 0, a, device_x, device_y);
+  // RUN: sh -c "test `grep -c -F 'hipLaunchKernel(HIP_KERNEL_NAME(axpy), dim3(1), dim3(kDataLen), 0, 0, a, device_x, device_y);' %t` -eq 2"
   axpy<<<1, kDataLen>>>(a, device_x, device_y);
 
   // Copy output data to host.
+  // RUN: sh -c "test `grep -c -F 'hipDeviceSynchronize();' %t` -eq 2"
   cudaDeviceSynchronize();
+  // RUN: sh -c "test `grep -c -F 'hipMemcpy(host_y, device_y, kDataLen * sizeof(float), hipMemcpyDeviceToHost);' %t` -eq 2"
   cudaMemcpy(host_y, device_y, kDataLen * sizeof(float), cudaMemcpyDeviceToHost);
 
   // Print the results.
@@ -35,6 +39,7 @@ int main(int argc, char* argv[]) {
     std::cout << "y[" << i << "] = " << host_y[i] << "\n";
   }
 
+  // RUN: sh -c "test `grep -c -F 'hipDeviceReset();' %t` -eq 2"
   cudaDeviceReset();
   return 0;
 }
