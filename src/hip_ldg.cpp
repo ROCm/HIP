ba59ea87ab60757c36a96a84e71397748ad64f27
Aditya Atluri <adityaavinash1@gmail.com>
2016-04-21 11:17:26 -0500

    added full data type support for __ldg

--- src/hip_ldg.cpp
+++ src/hip_ldg.cpp
@@ -19,43 +19,300 @@ THE SOFTWARE.
 
 #include"hcc_detail/hip_ldg.h"
 
-__device__ char __ldg(const char* ptr)
-{
-    return *ptr;
-}
-
-__device__ signed char __ldg(const signed char* ptr)
+__device__ char                 __ldg(const char* ptr)
 {
     return ptr[0];
 }
 
-__device__ short __ldg(const short* ptr)
+__device__ char1                __ldg(const char1* ptr)
 {
     return ptr[0];
 }
 
-__device__ int __ldg(const int* ptr)
+__device__ char2                __ldg(const char2* ptr)
 {
     return ptr[0];
 }
 
-__device__ long long __ldg(const long long* ptr)
+__device__ char3                __ldg(const char3* ptr)
+{
+    return ptr[0];
+}
+
+__device__ char4                __ldg(const char4* ptr)
+{
+    return ptr[0];
+}
+
+__device__ signed char          __ldg(const signed char* ptr)
+{
+    return ptr[0];
+}
+
+__device__ unsigned char        __ldg(const unsigned char* ptr)
 {
     return ptr[0];
 }
 
 
-__device__ int2 __ldg(const int2* ptr)
+__device__ short                __ldg(const short* ptr)
 {
     return ptr[0];
 }
 
-__device__ int4 __ldg(const int4* ptr)
+__device__ short1               __ldg(const short1* ptr)
 {
     return ptr[0];
 }
 
-__device__ float __ldg(const float* ptr)
+__device__ short2               __ldg(const short2* ptr)
 {
     return ptr[0];
 }
+
+__device__ short3               __ldg(const short3* ptr)
+{
+    return ptr[0];
+}
+
+__device__ short4               __ldg(const short4* ptr)
+{
+    return ptr[0];
+}
+
+__device__ unsigned short       __ldg(const unsigned short* ptr)
+{
+    return ptr[0];
+}
+
+
+__device__ int                  __ldg(const int* ptr)
+{
+    return ptr[0];
+}
+
+__device__ int1                 __ldg(const int1* ptr)
+{
+    return ptr[0];
+}
+
+__device__ int2                 __ldg(const int2* ptr)
+{
+    return ptr[0];
+}
+
+__device__ int3                 __ldg(const int3* ptr)
+{
+    return ptr[0];
+}
+
+__device__ int4                 __ldg(const int4* ptr)
+{
+    return ptr[0];
+}
+
+__device__ unsigned int         __ldg(const unsigned int* ptr)
+{
+    return ptr[0];
+}
+
+
+__device__ long                 __ldg(const long* ptr)
+{
+    return ptr[0];
+}
+
+__device__ long1                __ldg(const long1* ptr)
+{
+    return ptr[0];
+}
+
+__device__ long2                __ldg(const long2* ptr)
+{
+    return ptr[0];
+}
+
+__device__ long3                __ldg(const long3* ptr)
+{
+    return ptr[0];
+}
+
+__device__ long4                __ldg(const long4* ptr)
+{
+    return ptr[0];
+}
+
+__device__ unsigned long        __ldg(const unsigned long* ptr)
+{
+    return ptr[0];
+}
+
+
+__device__ long long            __ldg(const long long* ptr)
+{
+    return ptr[0];
+}
+
+__device__ longlong1            __ldg(const longlong1* ptr)
+{
+    return ptr[0];
+}
+
+__device__ longlong2            __ldg(const longlong2* ptr)
+{
+    return ptr[0];
+}
+
+__device__ longlong3            __ldg(const longlong3* ptr)
+{
+    return ptr[0];
+}
+
+__device__ longlong4            __ldg(const longlong4* ptr)
+{
+    return ptr[0];
+}
+
+__device__ unsigned long long   __ldg(const unsigned long long* ptr)
+{
+    return ptr[0];
+}
+
+
+__device__ uchar1               __ldg(const uchar1* ptr)
+{
+    return ptr[0];
+}
+
+__device__ uchar2               __ldg(const uchar2* ptr)
+{
+    return ptr[0];
+}
+
+__device__ uchar3               __ldg(const uchar3* ptr)
+{
+    return ptr[0];
+}
+
+__device__ uchar4               __ldg(const uchar4* ptr)
+{
+    return ptr[0];
+}
+
+
+__device__ ushort1              __ldg(const ushort1* ptr)
+{
+    return ptr[0];
+}
+
+__device__ ushort2              __ldg(const ushort2* ptr)
+{
+    return ptr[0];
+}
+
+__device__ ushort3              __ldg(const ushort3* ptr)
+{
+    return ptr[0];
+}
+
+__device__ ushort4              __ldg(const ushort4* ptr)
+{
+    return ptr[0];
+}
+
+
+__device__ uint1                __ldg(const uint1* ptr)
+{
+    return ptr[0];
+}
+
+__device__ uint2                __ldg(const uint2* ptr)
+{
+    return ptr[0];
+}
+
+__device__ uint3                __ldg(const uint3* ptr)
+{
+    return ptr[0];
+}
+
+__device__ uint4                __ldg(const uint4* ptr)
+{
+    return ptr[0];
+}
+
+
+__device__ ulonglong1           __ldg(const ulonglong1* ptr)
+{
+    return ptr[0];
+}
+
+__device__ ulonglong2           __ldg(const ulonglong2* ptr)
+{
+    return ptr[0];
+}
+
+__device__ ulonglong3           __ldg(const ulonglong3* ptr)
+{
+    return ptr[0];
+}
+
+__device__ ulonglong4           __ldg(const ulonglong4* ptr)
+{
+    return ptr[0];
+}
+
+
+__device__ float                __ldg(const float* ptr)
+{
+    return ptr[0];
+}
+
+__device__ float1               __ldg(const float1* ptr)
+{
+    return ptr[0];
+}
+
+__device__ float2               __ldg(const float2* ptr)
+{
+    return ptr[0];
+}
+
+__device__ float3               __ldg(const float3* ptr)
+{
+    return ptr[0];
+}
+
+__device__ float4               __ldg(const float4* ptr)
+{
+    return ptr[0];
+}
+
+
+__device__ double               __ldg(const double* ptr)
+{
+    return ptr[0];
+}
+
+__device__ double1              __ldg(const double1* ptr)
+{
+    return ptr[0];
+}
+
+__device__ double2              __ldg(const double2* ptr)
+{
+    return ptr[0];
+}
+
+__device__ double3              __ldg(const double3* ptr)
+{
+    return ptr[0];
+}
+
+__device__ double4              __ldg(const double4* ptr)
+{
+    return ptr[0];
+}
+
+
+
