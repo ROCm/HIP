880972860e2431adb0def0c2585f6a6c181767ef
Ben Sander <ben.sander@amd.com>
2016-06-16 09:29:03 -0500

    Update launch_bounds test
    [ROCm/hip commit: be3b79409a78aa7fc3b479ff57edcfbbb83f3eac]

--- tests/src/launch_bounds/hip_launch_bounds.cpp
+++ tests/src/launch_bounds/hip_launch_bounds.cpp
@@ -19,51 +19,98 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-// Simple test for memset.
-// Also serves as a template for other tests.
+
+
+// Test launch bounds and initialization conditions.
 
 #include "hip_runtime.h"
 #include "test_common.h"
 
+int p_blockSize = 256;
+
 
 __global__ 
 void
-myKern(hipLaunchParm lp, int *C, const int *A, int N)
+__launch_bounds__(256, 2)
+myKern(hipLaunchParm lp, int *C, const int *A, int N, int xfactor)
 {
-    int tid = hipThreadIdx_x;
+    int tid = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x);
 
-    C[tid] = A[tid];
+    if (tid < N) {
+        C[tid] = A[tid];
+    }
 };
+
+
+void parseMyArguments(int argc, char *argv[])
+{
+    int more_argc = HipTest::parseStandardArguments(argc, argv, false);
+    // parse args for this test:
+    for (int i = 1; i < more_argc; i++) {
+        const char *arg = argv[i];
+
+        if (!strcmp(arg, "--blockSize")) {
+            if (++i >= argc || !HipTest::parseInt(argv[i], &p_blockSize)) {
+               failed("Bad peerDevice argument");
+            }
+        } else {
+            failed("Bad argument '%s'", arg);
+        }
+    };
+};
+
 
 
 
 int main(int argc, char *argv[])
 {
-    HipTest::parseStandardArguments(argc, argv, true);
+    parseMyArguments(argc, argv);
 
     size_t Nbytes = N*sizeof(int);
 
     int *A_d, *C_d, *A_h, *C_h;
     HIPCHECK ( hipMalloc(&A_d, Nbytes) );
     HIPCHECK ( hipMalloc(&C_d, Nbytes) );
-    A_h = (int*)malloc (Nbytes);
 
+    A_h = (int*)malloc (Nbytes);
     C_h = (int*)malloc (Nbytes);
+
     for (int i=0; i<N; i++) {
         A_h[i] = i*10;
         C_h[i] = 0x0;
     }
 
-    HIPCHECK ( hipMemcpy(A_d, A_h, Nbytes, hipMemcpyHostToDevice) );
+    int blocks = N / p_blockSize;
+    printf ("running with N=%zu p_blockSize=%d blocks=%d\n", N, p_blockSize, blocks);
 
-    hipLaunchKernel(myKern, dim3(N), dim3(256), 0, 0, A_d, C_d, N);
+    HIPCHECK ( hipMemcpy(A_d, A_h, Nbytes, hipMemcpyHostToDevice) );
+    HIPCHECK ( hipGetLastError() );
+
+    hipLaunchKernel(myKern, dim3(blocks), dim3(p_blockSize), 0, 0, C_d, A_d, N, 0);
+
+    cudaFuncAttributes attrib;
+    cudaFuncGetAttributes (&attrib, myKern);
+    printf ("binaryVersion = %d\n", attrib.binaryVersion);
+    printf ("cacheModeCA = %d\n", attrib.cacheModeCA);
+    printf ("constSizeBytes = %zu\n", attrib.constSizeBytes);
+    printf ("localSizeBytes = %zud\n", attrib.localSizeBytes);
+    printf ("maxThreadsPerBlock = %d\n", attrib.maxThreadsPerBlock);
+    printf ("numRegs = %d\n", attrib.numRegs);
+    printf ("ptxVersion = %d\n", attrib.ptxVersion);
+    printf ("sharedSizeBytes = %zud\n", attrib.sharedSizeBytes);
+
+    HIPCHECK ( hipDeviceSynchronize() );
+
+    HIPCHECK ( hipGetLastError() );
 
     HIPCHECK ( hipMemcpy(C_h, C_d, Nbytes, hipMemcpyDeviceToHost) );
 
+    HIPCHECK ( hipDeviceSynchronize() );
+
     for (int i=0; i<N; i++) {
         int goldVal = i * 10;
-        if (A_h[i] != goldVal) {
-            failed("mismatch at index:%d computed:%02x, gold:%02x\n", i, (int)A_h[i], (int)goldVal);
+        if (C_h[i] != goldVal) {
+            failed("mismatch at index:%d computed:%02d, gold:%02d\n", i, (int)C_h[i], (int)goldVal);
 
         }
     }
