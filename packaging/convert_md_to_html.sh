1bb94cac7f4529bb02beff608db81b21508809c8
Maneesh Gupta <maneesh.gupta@amd.com>
2016-07-19 14:30:03 +0530

    Add markdown documentation to hip_docs package
    Change-Id: I4075e6baaf287356ac8b485cccc231bfb729f078
    
    
    [ROCm/hip commit: ab0bea5b151d31194e9ab904e82d4e3df5f42967]

---
+++ packaging/convert_md_to_html.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+function die {
+    echo "${1-Died}." >&2
+    exit 1
+}
+
+function cleanup {
+    rm -rf "$workdir"
+}
+
+# parse arguments
+hip_srcdir=$1
+html_destdir=$2
+[ "$hip_srcdir" != "" ] || [ "$html_destdir" != "" ] || die "Invalid arguments!"
+
+# create temporary directory for grip settings
+workdir=`mktemp -d`
+trap cleanup EXIT
+
+# setup grip
+export GRIPURL=$hip_srcdir
+export GRIPHOME=$workdir
+echo "CACHE_DIRECTORY = '$html_destdir/asset'" > $workdir/settings.py
+mkdir -p $html_destdir $html_destdir/clang-hipify $html_destdir/docs/markdown
+
+# convert all md files to html
+pushd $hip_srcdir
+for f in *.md clang-hipify/*.md docs/markdown/*.md; do grip --export --no-inline $f $html_destdir/${f%.*}.html; done
+popd
+
+# convert absolute links to relative links
+pushd $html_destdir
+for f in *.html; do sed -i "s?$GRIPURL/??g" $f; done
+for f in clang-hipify/*.html; do sed -i "s?$GRIPURL/?../?g" $f; done
+for f in docs/markdown/*.html; do sed -i "s?$GRIPURL/?../../?g" $f; done
+popd
+
+# update document titles
+pushd $html_destdir
+for f in *.html; do sed -i "s?.md - Grip??g" $f; done
+for f in clang-hipify/*.html; do sed -i "s?.md - Grip??g" $f; done
+for f in docs/markdown/*.html; do sed -i "s?.md - Grip??g" $f; done
+popd
+
+# replace .md with .html in links
+pushd $html_destdir
+for f in *.html; do sed -i "s?.md\"?.html\"?g" $f; done
+for f in *.html; do sed -i "s?.md#?.html#?g" $f; done
+for f in clang-hipify/*.html; do sed -i "s?.md\"?.html\"?g" $f; done
+for f in clang-hipify/*.html; do sed -i "s?.md#?.html#?g" $f; done
+for f in docs/markdown/*.html; do sed -i "s?.md\"?.html\"?g" $f; done
+for f in docs/markdown/*.html; do sed -i "s?.md#?.html#?g" $f; done
+popd
+
+# replace github.io links
+pushd $html_destdir
+sed -i "s?http://gpuopen-professionalcompute-tools.github.io/HIP?docs/RuntimeAPI/html/index.html?g" README.html
+sed -i "s?http://gpuopen-professionalcompute-tools.github.io/HIP?docs/RuntimeAPI/html/?g" RELEASE.html
+popd
+
+exit 0
