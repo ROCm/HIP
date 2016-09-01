4ef8b2524a51dd9b2ecf80f541003189c17f6c0e
Maneesh Gupta <maneesh.gupta@amd.com>
2016-09-01 12:13:11 +0530

    hipgenisa.sh looks for hipcc relative to itself
    Change-Id: Iac63fb5a87db1d735dbfa9697ce1f769a36b4d4a
    
    
    [ROCm/hip commit: 5456668776d2097ead89c81bec6d7be36cf6a7d0]

--- bin/hipgenisa.sh
+++ bin/hipgenisa.sh
@@ -16,13 +16,16 @@ then
   TARGET=${GEN_ISA:13:12}
 fi
 
+SOURCE="${BASH_SOURCE[0]}"
+HIP_PATH="$( command cd -P "$( dirname "$SOURCE" )/.." && pwd )"
+
 export KMDUMPISA=1
 export KMDUMPLLVM=1
 mkdir /tmp/hipgenisa
-hipcc $FILE_NAMES -o /tmp/hipgenisa/a.out
+$HIP_PATH/bin/hipcc $FILE_NAMES -o /tmp/hipgenisa/a.out
 mv dump.* /tmp/hipgenisa/
-${ROCM_PATH}/hcc-lc/bin/llvm-mc -arch=amdgcn -mcpu=$TARGET -filetype=obj /tmp/hipgenisa/dump.isa -o /tmp/hipgenisa/dump.o
-${ROCM_PATH}/llvm/bin/clang -target amdgcn--amdhsa /tmp/hipgenisa/dump.o -o $OUTPUT_FILE
+$ROCM_PATH/hcc-lc/bin/llvm-mc -arch=amdgcn -mcpu=$TARGET -filetype=obj /tmp/hipgenisa/dump.isa -o /tmp/hipgenisa/dump.o
+$ROCM_PATH/llvm/bin/clang -target amdgcn--amdhsa /tmp/hipgenisa/dump.o -o $OUTPUT_FILE
 rm -r /tmp/hipgenisa
 export KMDUMPISA=0
 export KMDUMPLLVM=0
