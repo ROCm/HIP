c0cfdf2ee98a7bb6df3c6726fdd5a4d1ff704791
Aditya Atluri <Aditya.Atluri@amd.com>
2016-08-31 20:16:48 -0500

    corrected path to hipgenisa.sh and fixed its working
    Change-Id: I140055d5e800cd4b23253171ba889db5e63b637b

--- bin/hipgenisa.sh
+++ bin/hipgenisa.sh
@@ -18,9 +18,11 @@ fi
 
 export KMDUMPISA=1
 export KMDUMPLLVM=1
+mkdir /tmp/hipgenisa
 hipcc $FILE_NAMES -o /tmp/hipgenisa/a.out
 mv dump.* /tmp/hipgenisa/
 ${ROCM_PATH}/hcc-lc/bin/llvm-mc -arch=amdgcn -mcpu=$TARGET -filetype=obj /tmp/hipgenisa/dump.isa -o /tmp/hipgenisa/dump.o
 ${ROCM_PATH}/llvm/bin/clang -target amdgcn--amdhsa /tmp/hipgenisa/dump.o -o $OUTPUT_FILE
+rm -r /tmp/hipgenisa
 export KMDUMPISA=0
 export KMDUMPLLVM=0
