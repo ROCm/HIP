ba8a05a3c0e072a37b434f32a263c78c872661ed
Maneesh Gupta <maneesh.gupta@amd.com>
2016-09-04 13:49:43 +0530

    module_api sample: no longer need EXTERN_C workaround
    Change-Id: Ida087d832df8e1f3620b38f920ec2853aad641c8

--- samples/0_Intro/module_api/vcpy_kernel.cpp
+++ samples/0_Intro/module_api/vcpy_kernel.cpp
@@ -1,12 +1,6 @@
-#include<hip_runtime.h>
+#include "hip/hip_runtime.h"
 
-#ifdef __HIP_PLATFORM_NVCC__
-#define EXTERN_C extern "C"
-#else
-#define EXTERN_C
-#endif
-
-EXTERN_C __global__ void hello_world(hipLaunchParm lp, float *a, float *b)
+extern "C" __global__ void hello_world(hipLaunchParm lp, float *a, float *b)
 {
     int tx = hipThreadIdx_x;
     b[tx] = a[tx];
