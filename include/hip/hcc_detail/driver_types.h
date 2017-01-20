22acd654cf788dd3435d41b13f376617ec9a10b2
Aditya Atluri <Aditya.Atluri@amd.com>
2017-01-20 16:54:48 -0600

    fixed hipArray issues
    1. Fixed build issues produced from previous commit
    2. Create new header files to manage data structures better
    
    Change-Id: I704d82c196c1858ed7617d76e40612eb507d2aa0

--- include/hip/hcc_detail/driver_types.h
+++ include/hip/hcc_detail/driver_types.h
@@ -1,16 +1,13 @@
 /*
-Copyright (c) 2015-2017 Advanced Micro Devices, Inc. All rights reserved.
-
+Copyright (c) 2015-2016 Advanced Micro Devices, Inc. All rights reserved.
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -20,17 +17,25 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-#pragma once
+#ifndef HIP_HCC_DETAIL_DRIVER_TYPES_H
+#define HIP_HCC_DETAIL_DRIVER_TYPES_H
 
-// Some standard header files, these are included by hc.hpp and so want to make them avail on both
-// paths to provide a consistent include env and avoid "missing symbol" errors that only appears
-// on NVCC path:
+enum hipChannelFormatKind
+{
+  hipChannelFormatKindSigned = 0,
+  hipChannelFormatKindUnsigned = 1,
+  hipChannelFormatKindFloat = 2,
+  hipChannelFormatKindNone = 3
+};
+
+struct hipChannelFormatDesc
+{
+  int x;
+  int y;
+  int z;
+  int w;
+  enum hipChannelFormatKind f;
+};
 
 
-#if defined(__HIP_PLATFORM_HCC__) && !defined (__HIP_PLATFORM_NVCC__)
-#include <hip/hcc_detail/math_functions.h>
-#elif defined(__HIP_PLATFORM_NVCC__) && !defined (__HIP_PLATFORM_HCC__)
-#include <hip/nvcc_detail/math_functions.h>
-#else
-#error("Must define exactly one of __HIP_PLATFORM_HCC__ or __HIP_PLATFORM_NVCC__");
 #endif
