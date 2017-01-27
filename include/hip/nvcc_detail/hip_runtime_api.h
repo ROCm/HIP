3e21d55c98c72b934500d977d7c64b4c84edce71
Rahul Garg <rahul.garg@amd.com>
2017-01-27 14:32:08 +0530

    hipMallocArray fixes for NV path
    Change-Id: I1ca43e6bc0cd405998888005c20dfb1ea57003d5

--- include/hip/nvcc_detail/hip_runtime_api.h
+++ include/hip/nvcc_detail/hip_runtime_api.h
@@ -219,8 +219,8 @@ inline static hipError_t hipHostMalloc(void** ptr, size_t size, unsigned int fla
 	return hipCUDAErrorTohipError(cudaHostAlloc(ptr, size, flags));
 }
 
-inline static hipError_t hipMallocArray(hipArray** array, const hipChannelFormatDesc* desc, size_width, size_t height, unsigned int flags) {
-  return hipCUDAErrorTohipError(cudaMallocArray(array, desc, size_width, height, flags));
+inline static hipError_t hipMallocArray(hipArray** array, const hipChannelFormatDesc* desc, size_t width, size_t height, unsigned int flags) {
+  return hipCUDAErrorTohipError(cudaMallocArray(array, desc, width, height, flags));
 }
 
 inline static hipError_t hipFreeArray(hipArray* array) {
@@ -334,7 +334,7 @@ inline static hipError_t hipMemcpyToSymbolAsync(const void* symbol, const void* 
 }
 
 inline static hipError_t hipMemcpy2D(void* dst, size_t dpitch, const void* src, size_t spitch, size_t width, size_t height, hipMemcpyKind kind){
-  return hipCUDAErrorTohipError(cudaMemcpy2D(dst, dpitch, src, width, height, hipMemcpyKindToCudaMemcpyKind(Kind)));
+  return hipCUDAErrorTohipError(cudaMemcpy2D(dst, dpitch, src, spitch, width, height, hipMemcpyKindToCudaMemcpyKind(kind)));
 }
 
 inline static hipError_t hipMemcpy2DToArray(hipArray *dst, size_t wOffset, size_t hOffset, const void* src, size_t spitch, size_t width, size_t height, hipMemcpyKind kind){
@@ -342,7 +342,7 @@ inline static hipError_t hipMemcpy2DToArray(hipArray *dst, size_t wOffset, size_
 }
 
 inline static hipError_t hipMemcpyToArray(hipArray* dst, size_t wOffset, size_t hOffset, const void* src, size_t count, hipMemcpyKind kind) {
-  return hipCUDAErrorTohipError(cudaMemcpy2DToArray(dst, wOffset, hOffset, src, count, hipMemcpyKindToCudaMemcpyKind(kind)));
+  return hipCUDAErrorTohipError(cudaMemcpyToArray(dst, wOffset, hOffset, src, count, hipMemcpyKindToCudaMemcpyKind(kind)));
 }
 
 inline static hipError_t hipDeviceSynchronize() {
