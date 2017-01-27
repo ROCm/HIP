37ef1c695de5dc9409366e9a903f9c29828fa77d
Ben Sander <ben.sander@amd.com>
2017-01-27 11:21:08 -0600

    Add HIP_TRACE_API=4.  Only display memory allocation/free apis.
    [ROCm/hip commit: 0edab14139c564781e85febb603957a09590f9b6]

--- src/hip_hcc.cpp
+++ src/hip_hcc.cpp
@@ -1435,7 +1435,7 @@ hipStream_t ihipSyncAndResolveStream(hipStream_t stream)
 void ihipPrintKernelLaunch(const char *kernelName, const grid_launch_parm *lp, const hipStream_t stream)
 {
 
-    if ((HIP_TRACE_API & (1<<TRACE_CMD)) || HIP_PROFILE_API || (COMPILE_HIP_DB && HIP_TRACE_API)) {
+    if ((HIP_TRACE_API & (1<<TRACE_KCMD)) || HIP_PROFILE_API || (COMPILE_HIP_DB & HIP_TRACE_API)) {
         std::stringstream os_pre;
         std::stringstream os;
         os_pre  << "<<hip-api tid:";
