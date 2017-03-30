071f19521cea41bca8ecf7ec52c9081054317c01
Sun, Peng <peng.sun@amd.com>
2017-03-30 18:16:56 -0500

    remove extra GGL header info
    Change-Id: I09f0d1b64a7a31eb2e926f19b69b7bafbacc7787

--- include/hip/hcc_detail/concepts.hpp
+++ include/hip/hcc_detail/concepts.hpp
@@ -20,9 +20,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-//
-// Created by alexv on 25/10/16.
-//
 #pragma once
 
 namespace glo_tests // Documentation only.
