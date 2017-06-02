4d035caedfdf02340a1e8140d0e16a033e7bb87a
emankov <evgeniy.mankov@amd.com>
2017-06-02 16:30:43 +0300

    [HIPIFY] rename legacy hipify perl script and its usage to hipify-perl

--- bin/hipexamine-perl.sh
+++ bin/hipexamine-perl.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#usage : hipexamine.sh DIRNAME [hipify.pl options]
+#usage : hipexamine.sh DIRNAME [hipify-perl options]
 
-# Generate HIP stats (LOC, CUDA->API conversions, missing functionality) for all the code files 
+# Generate HIP stats (LOC, CUDA->API conversions, missing functionality) for all the code files
 # in the specified directory.
 
 
 SCRIPT_DIR=`dirname $0`
 SEARCH_DIR=$1
 shift
-$SCRIPT_DIR/hipify -no-output -print-stats "$@" `$SCRIPT_DIR/findcode.sh $SEARCH_DIR`
+$SCRIPT_DIR/hipify-perl -no-output -print-stats "$@" `$SCRIPT_DIR/findcode.sh $SEARCH_DIR`
