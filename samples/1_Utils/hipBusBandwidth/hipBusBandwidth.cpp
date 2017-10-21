0d1ad06458cc6b8a4953b1fb5bc8f6083d8330f6
Ben Sander <ben.sander@amd.com>
2017-10-21 07:47:32 -0500

    Use 2X for bidir memory bandwidth calc

--- samples/1_Utils/hipBusBandwidth/hipBusBandwidth.cpp
+++ samples/1_Utils/hipBusBandwidth/hipBusBandwidth.cpp
@@ -594,7 +594,7 @@ void RunBenchmark_Bidir(ResultDatabase &resultDB)
                         " ms\n";
             }
 
-            double speed = (double(sizeToBytes(thisSize)) / (1000*1000)) / t;
+            double speed = (double(sizeToBytes(2*thisSize)) / (1000*1000)) / t;
             char sizeStr[256];
             sprintf(sizeStr, "%9s", sizeToString(thisSize).c_str());
             resultDB.AddResult(std::string("Bidir_Bandwidth") + "_" + mallocModeString(p_malloc_mode), sizeStr, "GB/sec", speed);
