36a03de9d84a74d61f62afe45e857e78975ee54a
Chris Kitching <chriskitching@linux.com>
2017-10-25 19:23:32 +0100

    Move string utility functions into their own translation unit
    [ROCm/hip commit: ee8e11a72094456697021a74bff05b5dbf2e4e11]

---
+++ hipify-clang/src/StringUtils.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include "llvm/ADT/StringRef.h"
+
+/**
+ * Remove double-quotes from the start/end of a string, if present.
+ */
+llvm::StringRef unquoteStr(llvm::StringRef s);
+
+/**
+ * If `s` starts with `prefix`, remove it. Otherwise, does nothing.
+ */
+void removePrefixIfPresent(std::string &s, std::string prefix);
