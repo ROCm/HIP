af6b75bb2a749a20828b17609f81d05ed8075cb9
Evgeny Mankov <Evgeniy.Mankov@amd.com>
2017-12-21 20:21:28 +0300

    [HIPIFY][tests] CUDA_SDK_Samples\0_Simple\simpleTexture is added

---
+++ tests/hipify-clang/CUDA_SDK_Samples/0_Simple/simpleTexture/simpleTexture.cu
@@ -0,0 +1,286 @@
+// RUN: %run_test hipify "%s" "%t" %cuda_args
+/*
+ * Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+/*
+ * This sample demonstrates how use texture fetches in CUDA
+ *
+ * This sample takes an input PGM image (image_filename) and generates
+ * an output PGM image (image_filename_out).  This CUDA kernel performs
+ * a simple 2D transform (rotation) on the texture coordinates (u,v).
+ */
+
+// Includes, system
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#ifdef _WIN32
+#  define WINDOWS_LEAN_AND_MEAN
+#  define NOMINMAX
+#  include <windows.h>
+#endif
+
+// Includes CUDA
+
+// CHECK: #include <hip/hip_runtime.h>
+#include <cuda_runtime.h>
+#include <texture_fetch_functions.h>
+
+// Utilities and timing functions
+#include <helper_functions.h>    // includes cuda.h and cuda_runtime_api.h
+
+// CUDA helper functions
+#include <helper_cuda.h>         // helper functions for CUDA error check
+
+#define MAX_EPSILON_ERROR 5e-3f
+
+// Define the files that are to be save and the reference images for validation
+const char *imageFilename = "lena_bw.pgm";
+const char *refFilename   = "ref_rotated.pgm";
+
+const char *sampleName = "simpleTexture";
+
+////////////////////////////////////////////////////////////////////////////////
+// Constants
+const float angle = 0.5f;        // angle to rotate image by (in radians)
+
+// Texture reference for 2D float texture
+// CHECK: texture<float, 2, hipReadModeElementType> tex;
+texture<float, 2, cudaReadModeElementType> tex;
+
+// Auto-Verification Code
+bool testResult = true;
+
+////////////////////////////////////////////////////////////////////////////////
+//! Transform an image using texture lookups
+//! @param outputData  output data in global memory
+////////////////////////////////////////////////////////////////////////////////
+__global__ void transformKernel(float *outputData,
+                                int width,
+                                int height,
+                                float theta)
+{
+    // calculate normalized texture coordinates
+    unsigned int x = blockIdx.x*blockDim.x + threadIdx.x;
+    unsigned int y = blockIdx.y*blockDim.y + threadIdx.y;
+
+    float u = (float)x - (float)width/2; 
+    float v = (float)y - (float)height/2; 
+    float tu = u*cosf(theta) - v*sinf(theta); 
+    float tv = v*cosf(theta) + u*sinf(theta); 
+
+    tu /= (float)width; 
+    tv /= (float)height; 
+
+    // read from texture and write to global memory
+    outputData[y*width + x] = tex2D(tex, tu+0.5f, tv+0.5f);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Declaration, forward
+void runTest(int argc, char **argv);
+
+////////////////////////////////////////////////////////////////////////////////
+// Program main
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+    printf("%s starting...\n", sampleName);
+
+    // Process command-line arguments
+    if (argc > 1)
+    {
+        if (checkCmdLineFlag(argc, (const char **) argv, "input"))
+        {
+            getCmdLineArgumentString(argc,
+                                     (const char **) argv,
+                                     "input",
+                                     (char **) &imageFilename);
+
+            if (checkCmdLineFlag(argc, (const char **) argv, "reference"))
+            {
+                getCmdLineArgumentString(argc,
+                                         (const char **) argv,
+                                         "reference",
+                                         (char **) &refFilename);
+            }
+            else
+            {
+                printf("-input flag should be used with -reference flag");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else if (checkCmdLineFlag(argc, (const char **) argv, "reference"))
+        {
+            printf("-reference flag should be used with -input flag");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    runTest(argc, argv);
+
+    printf("%s completed, returned %s\n",
+           sampleName,
+           testResult ? "OK" : "ERROR!");
+    exit(testResult ? EXIT_SUCCESS : EXIT_FAILURE);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Run a simple test for CUDA
+////////////////////////////////////////////////////////////////////////////////
+void runTest(int argc, char **argv)
+{
+    int devID = findCudaDevice(argc, (const char **) argv);
+
+    // load image from disk
+    float *hData = NULL;
+    unsigned int width, height;
+    char *imagePath = sdkFindFilePath(imageFilename, argv[0]);
+
+    if (imagePath == NULL)
+    {
+        printf("Unable to source image file: %s\n", imageFilename);
+        exit(EXIT_FAILURE);
+    }
+
+    sdkLoadPGM(imagePath, &hData, &width, &height);
+
+    unsigned int size = width * height * sizeof(float);
+    printf("Loaded '%s', %d x %d pixels\n", imageFilename, width, height);
+
+    //Load reference image from image (output)
+    float *hDataRef = (float *) malloc(size);
+    char *refPath = sdkFindFilePath(refFilename, argv[0]);
+
+    if (refPath == NULL)
+    {
+        printf("Unable to find reference image file: %s\n", refFilename);
+        exit(EXIT_FAILURE);
+    }
+
+    sdkLoadPGM(refPath, &hDataRef, &width, &height);
+
+    // Allocate device memory for result
+    float *dData = NULL;
+    // CHECK: checkCudaErrors(hipMalloc((void **) &dData, size));
+    checkCudaErrors(cudaMalloc((void **) &dData, size));
+
+    // Allocate array and copy image data
+    // CHECK: hipChannelFormatDesc channelDesc =
+    cudaChannelFormatDesc channelDesc =
+    // CHECK: hipCreateChannelDesc(32, 0, 0, 0, hipChannelFormatKindFloat);
+        cudaCreateChannelDesc(32, 0, 0, 0, cudaChannelFormatKindFloat);
+    // CHECK: hipArray *cuArray;
+    cudaArray *cuArray;
+    // CHECK: checkCudaErrors(hipMallocArray(&cuArray,
+    checkCudaErrors(cudaMallocArray(&cuArray,
+                                    &channelDesc,
+                                    width,
+                                    height));
+    // CHECK: checkCudaErrors(hipMemcpyToArray(cuArray,
+    checkCudaErrors(cudaMemcpyToArray(cuArray,
+                                      0,
+                                      0,
+                                      hData,
+                                      size,
+                                      cudaMemcpyHostToDevice));
+
+    // Set texture parameters
+    // CHECK: tex.addressMode[0] = hipAddressModeWrap;
+    // CHECK: tex.addressMode[1] = hipAddressModeWrap;
+    // CHECK: tex.filterMode = hipFilterModeLinear;
+    tex.addressMode[0] = cudaAddressModeWrap;
+    tex.addressMode[1] = cudaAddressModeWrap;
+    tex.filterMode = cudaFilterModeLinear;
+    tex.normalized = true;    // access with normalized texture coordinates
+
+    // Bind the array to the texture
+    // CHECK: checkCudaErrors(hipBindTextureToArray(tex, cuArray, channelDesc));
+    checkCudaErrors(cudaBindTextureToArray(tex, cuArray, channelDesc));
+
+    dim3 dimBlock(8, 8, 1);
+    dim3 dimGrid(width / dimBlock.x, height / dimBlock.y, 1);
+
+    // Warmup
+    // CHECK: hipLaunchKernelGGL(transformKernel, dim3(dimGrid), dim3(dimBlock), 0, 0, dData, width, height, angle);
+    transformKernel<<<dimGrid, dimBlock, 0>>>(dData, width, height, angle);
+    // CHECK: checkCudaErrors(hipDeviceSynchronize());
+    checkCudaErrors(cudaDeviceSynchronize());
+    StopWatchInterface *timer = NULL;
+    sdkCreateTimer(&timer);
+    sdkStartTimer(&timer);
+
+    // Execute the kernel
+    // CHECK: hipLaunchKernelGGL(transformKernel, dim3(dimGrid), dim3(dimBlock), 0, 0, dData, width, height, angle);
+    transformKernel<<<dimGrid, dimBlock, 0>>>(dData, width, height, angle);
+
+    // Check if kernel execution generated an error
+    getLastCudaError("Kernel execution failed");
+    // CHECK: checkCudaErrors(hipDeviceSynchronize());
+    checkCudaErrors(cudaDeviceSynchronize());
+    sdkStopTimer(&timer);
+    printf("Processing time: %f (ms)\n", sdkGetTimerValue(&timer));
+    printf("%.2f Mpixels/sec\n",
+           (width *height / (sdkGetTimerValue(&timer) / 1000.0f)) / 1e6);
+    sdkDeleteTimer(&timer);
+
+    // Allocate mem for the result on host side
+    float *hOutputData = (float *) malloc(size);
+    // copy result from device to host
+    // CHECK: checkCudaErrors(hipMemcpy(hOutputData,
+    checkCudaErrors(cudaMemcpy(hOutputData,
+                               dData,
+                               size,
+    // CHECK: hipMemcpyDeviceToHost));
+                               cudaMemcpyDeviceToHost));
+
+    // Write result to file
+    char outputFilename[1024];
+    strcpy(outputFilename, imagePath);
+    strcpy(outputFilename + strlen(imagePath) - 4, "_out.pgm");
+    sdkSavePGM(outputFilename, hOutputData, width, height);
+    printf("Wrote '%s'\n", outputFilename);
+
+    // Write regression file if necessary
+    if (checkCmdLineFlag(argc, (const char **) argv, "regression"))
+    {
+        // Write file for regression test
+        sdkWriteFile<float>("./data/regression.dat",
+                            hOutputData,
+                            width*height,
+                            0.0f,
+                            false);
+    }
+    else
+    {
+        // We need to reload the data from disk,
+        // because it is inverted upon output
+        sdkLoadPGM(outputFilename, &hOutputData, &width, &height);
+
+        printf("Comparing files\n");
+        printf("\toutput:    <%s>\n", outputFilename);
+        printf("\treference: <%s>\n", refPath);
+
+        testResult = compareData(hOutputData,
+                                 hDataRef,
+                                 width*height,
+                                 MAX_EPSILON_ERROR,
+                                 0.15f);
+    }
+    // CHECK: checkCudaErrors(hipFree(dData));
+    // CHECK: checkCudaErrors(hipFreeArray(cuArray));
+    checkCudaErrors(cudaFree(dData));
+    checkCudaErrors(cudaFreeArray(cuArray));
+    free(imagePath);
+    free(refPath);
+}
