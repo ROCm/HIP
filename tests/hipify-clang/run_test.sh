6500cce6aaaf691f6bb9c5e1b805b112b687ceb8
emankov <evgeniy.mankov@amd.com>
2018-01-15 14:20:37 +0300

    [HIPIFY][#311][fix] Get rid of socat in run_test.sh

--- tests/hipify-clang/run_test.sh
+++ tests/hipify-clang/run_test.sh
@@ -13,16 +13,5 @@ shift 3
 
 # Remaining args are the ones to forward to clang proper.
 
-# Time for the classic insane little trick for making colour output work.
-# A self-deleting shell-script that does the thing we want to do...
-TMP_SCRIPT=$(mktemp)
-cat << EOF > $TMP_SCRIPT
-set -o errexit
-set -o xtrace
-rm $TMP_SCRIPT
 $HIPIFY -o=$TMP_FILE $IN_FILE -- $@ && cat $TMP_FILE | sed -Ee 's|//.+|// |g' | FileCheck $IN_FILE
-EOF
-chmod a+x $TMP_SCRIPT
 
-# Run the script via socat, spawning a virtual terminal and propagating exit code, and hence failure.
-socat -du EXEC:$TMP_SCRIPT,pty,stderr STDOUT
