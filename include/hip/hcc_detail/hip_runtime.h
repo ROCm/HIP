f2f387a2f7ed8e2e845c4095a7901184e1c0af45
Yaxun Sam Liu <yaxun.liu@amd.com>
2018-09-07 16:15:08 -0400

    Add empty printf for hip-clang
    [ROCm/hip commit: 3e1833ca52b72dd9b0abef92b28e2b18391be8ae]

--- include/hip/hcc_detail/hip_runtime.h
+++ include/hip/hcc_detail/hip_runtime.h
@@ -264,19 +264,16 @@ extern "C" __device__ void* __hip_hc_free(void* ptr);
 static inline __device__ void* malloc(size_t size) { return __hip_hc_malloc(size); }
 static inline __device__ void* free(void* ptr) { return __hip_hc_free(ptr); }
 
-#ifdef __HCC_ACCELERATOR__
-
-#ifdef HC_FEATURE_PRINTF
+#if defined(__HCC_ACCELERATOR__) && defined(HC_FEATURE_PRINTF)
 template <typename... All>
 static inline __device__ void printf(const char* format, All... all) {
     hc::printf(format, all...);
 }
-#else
+#elif defined(__HCC_ACCELERATOR__) || __HIP__
 template <typename... All>
 static inline __device__ void printf(const char* format, All... all) {}
 #endif
 
-#endif
 #endif //__HCC_OR_HIP_CLANG__
 
 #ifdef __HCC__
