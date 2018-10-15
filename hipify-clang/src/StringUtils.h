0a8a8451956b09cc016bb29a8d5efeffcb582982
Evgeny Mankov <Evgeniy.Mankov@amd.com>
2018-10-15 15:27:37 +0300

    [HIPIFY] Code cleanup and formatting
    [ROCm/hip commit: ee3a5cc722fd582262b2c7d75cd49049680ff431]

--- hipify-clang/src/StringUtils.h
+++ hipify-clang/src/StringUtils.h
@@ -4,11 +4,11 @@
 #include "llvm/ADT/StringRef.h"
 
 /**
- * Remove double-quotes from the start/end of a string, if present.
- */
+  * Remove double-quotes from the start/end of a string, if present.
+  */
 llvm::StringRef unquoteStr(llvm::StringRef s);
 
 /**
- * If `s` starts with `prefix`, remove it. Otherwise, does nothing.
- */
+  * If `s` starts with `prefix`, remove it. Otherwise, does nothing.
+  */
 void removePrefixIfPresent(std::string &s, std::string prefix);
