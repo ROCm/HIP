6ebcc2922cbce135d7bd48a7e9623295e73d3b73
Michael Kuron <mkuron@users.noreply.github.com>
2018-11-06 20:46:30 +0100

    Use correct trace macro in hipGetSymbolAddress/hipGetSymbolSize

--- src/hip_memory.cpp
+++ src/hip_memory.cpp
@@ -1101,7 +1101,7 @@ hipError_t hipMemcpyFromSymbolAsync(void* dst, const void* symbolName, size_t co
 
 
 hipError_t hipGetSymbolAddress(void** devPtr, const void* symbolName) {
-    HIP_INIT_SPECIAL_API((TRACE_MCMD), devPtr, symbolName);
+    HIP_INIT_API(devPtr, symbolName);
 
     size_t size = 0;
     return ihipModuleGetGlobal(devPtr, &size, 0, static_cast<const char*>(symbolName));
@@ -1109,7 +1109,7 @@ hipError_t hipGetSymbolAddress(void** devPtr, const void* symbolName) {
 
 
 hipError_t hipGetSymbolSize(size_t* size, const void* symbolName) {
-    HIP_INIT_SPECIAL_API((TRACE_MCMD), size, symbolName);
+    HIP_INIT_API(size, symbolName);
 
     void* devPtr = nullptr;
     return ihipModuleGetGlobal(&devPtr, size, 0, static_cast<const char*>(symbolName));
