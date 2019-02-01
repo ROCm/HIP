7e5ebb16e1d74633ee8113071297c491ce2e8d83
foreman <dl.constructicon@amd.com>
2019-02-01 13:43:50 -0500

    P4 to Git Change 1738965 by cpaquot@cpaquot-ocl-lc-lnx on 2019/02/01 13:17:10
    	SWDEV-145570 - [HIP] Handle multi device setup
    
    Affected files ...
    
    ... //depot/stg/opencl/drivers/opencl/api/hip/hip_device_runtime.cpp#13 edit
    ... //depot/stg/opencl/drivers/opencl/api/hip/hip_internal.hpp#21 edit
    ... //depot/stg/opencl/drivers/opencl/api/hip/hip_platform.cpp#21 edit

--- api/hip/hip_internal.hpp
+++ api/hip/hip_internal.hpp
@@ -87,6 +87,7 @@ namespace hip {
 };
 extern std::vector<amd::Context*> g_devices;
 extern hipError_t ihipDeviceGetCount(int* count);
+extern int ihipGetDevice();
 extern amd::Memory* getMemoryObject(const void* ptr, size_t& offset);
 
 #define HIP_RETURN(ret)          \
