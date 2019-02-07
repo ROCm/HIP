06f4309b6a2064fc252f0880068eb224e45771ee
Evgeny Mankov <evgeny.mankov@gmail.com>
2019-02-07 19:25:23 +0300

    [HIPIFY][BLAS][tests] Add tests on hipifying to 'roc'

--- tests/hipify-clang/unit_tests/libraries/cuBLAS/rocBLAS/cublas_0_based_indexing_rocblas.cu
+++ tests/hipify-clang/unit_tests/libraries/cuBLAS/rocBLAS/cublas_0_based_indexing_rocblas.cu
@@ -1,24 +1,24 @@
-// RUN: %run_test hipify "%s" "%t" %hipify_args %clang_args
+// RUN: %run_test hipify "%s" "%t" %hipify_args "-roc" %clang_args
 
 // CHECK: #include <hip/hip_runtime.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-// CHECK: #include "hipblas.h"
+// CHECK: #include "rocblas.h"
 #include "cublas.h"
 #define M 6
 #define N 5
 #define IDX2C(i,j,ld) (((j)*(ld))+(i))
 static __inline__ void modify(float *m, int ldm, int n, int p, int q, float
   alpha, float beta) {
-  // CHECK: hipblasSscal(n - p, alpha, &m[IDX2C(p, q, ldm)], ldm);
-  // CHECK: hipblasSscal(ldm - p, beta, &m[IDX2C(p, q, ldm)], 1);
+  // CHECK: rocblas_sscal(n - p, alpha, &m[IDX2C(p, q, ldm)], ldm);
+  // CHECK: rocblas_sscal(ldm - p, beta, &m[IDX2C(p, q, ldm)], 1);
   cublasSscal(n - p, alpha, &m[IDX2C(p, q, ldm)], ldm);
   cublasSscal(ldm - p, beta, &m[IDX2C(p, q, ldm)], 1);
 }
 int main(void) {
   int i, j;
-  // CHECK: hipblasStatus_t stat;
+  // CHECK: rocblas_status stat;
   cublasStatus stat;
   float* devPtrA;
   float* a = 0;
@@ -36,16 +36,16 @@ int main(void) {
   cublasInit();
   // cublasAlloc is not supported yet
   stat = cublasAlloc(M*N, sizeof(*a), (void**)&devPtrA);
-  // CHECK: if (stat != HIPBLAS_STATUS_SUCCESS) {
+  // CHECK: if (stat != rocblas_status_success) {
   if (stat != CUBLAS_STATUS_SUCCESS) {
     printf("device memory allocation failed");
     // cublasShutdown is not supported yet
     cublasShutdown();
     return EXIT_FAILURE;
   }
-  // CHECK: stat = hipblasSetMatrix(M, N, sizeof(*a), a, M, devPtrA, M);
+  // CHECK: stat = rocblas_set_matrix(M, N, sizeof(*a), a, M, devPtrA, M);
   stat = cublasSetMatrix(M, N, sizeof(*a), a, M, devPtrA, M);
-  // CHECK: if (stat != HIPBLAS_STATUS_SUCCESS) {
+  // CHECK: if (stat != rocblas_status_success) {
   if (stat != CUBLAS_STATUS_SUCCESS) {
     printf("data download failed");
     // cublasFree is not supported yet
@@ -55,9 +55,9 @@ int main(void) {
     return EXIT_FAILURE;
   }
   modify(devPtrA, M, N, 1, 2, 16.0f, 12.0f);
-  // CHECK: stat = hipblasGetMatrix(M, N, sizeof(*a), devPtrA, M, a, M);
+  // CHECK: stat = rocblas_get_matrix(M, N, sizeof(*a), devPtrA, M, a, M);
   stat = cublasGetMatrix(M, N, sizeof(*a), devPtrA, M, a, M);
-  // CHECK: if (stat != HIPBLAS_STATUS_SUCCESS) {
+  // CHECK: if (stat != rocblas_status_success) {
   if (stat != CUBLAS_STATUS_SUCCESS) {
     printf("data upload failed");
     // cublasFree is not supported yet
---
+++ tests/hipify-clang/unit_tests/libraries/cuBLAS/rocBLAS/cublas_1_based_indexing_rocblas.cu
@@ -0,0 +1,90 @@
+// RUN: %run_test hipify "%s" "%t" %hipify_args "-roc" %clang_args
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+// CHECK: #include <hip/hip_runtime.h>
+#include <cuda_runtime.h>
+// CHECK: #include "rocblas.h"
+#include "cublas_v2.h"
+#define M 6
+#define N 5
+#define IDX2F(i,j,ld) ((((j)-1)*(ld))+((i)-1))
+// CHECK: static __inline__ void modify(rocblas_handle handle, float *m, int ldm, int
+static __inline__ void modify(cublasHandle_t handle, float *m, int ldm, int
+  n, int p, int q, float alpha, float beta) {
+  // CHECK: rocblas_sscal(handle, n - p + 1, &alpha, &m[IDX2F(p, q, ldm)], ldm);
+  // CHECK: rocblas_sscal(handle, ldm - p + 1, &beta, &m[IDX2F(p, q, ldm)], 1);
+  cublasSscal(handle, n - p + 1, &alpha, &m[IDX2F(p, q, ldm)], ldm);
+  cublasSscal(handle, ldm - p + 1, &beta, &m[IDX2F(p, q, ldm)], 1);
+}
+int main(void) {
+  // CHECK: hipError_t cudaStat;
+  // CHECK: rocblas_status stat;
+  // CHECK: rocblas_handle handle;
+  cudaError_t cudaStat;
+  cublasStatus_t stat;
+  cublasHandle_t handle;
+  int i, j;
+  float* devPtrA;
+  float* a = 0;
+  a = (float *)malloc(M * N * sizeof(*a));
+  if (!a) {
+    printf("host memory allocation failed");
+    return EXIT_FAILURE;
+  }
+  for (j = 1; j <= N; j++) {
+    for (i = 1; i <= M; i++) {
+      a[IDX2F(i, j, M)] = (float)((i - 1) * M + j);
+    }
+  }
+  // CHECK: cudaStat = hipMalloc((void**)&devPtrA, M*N * sizeof(*a));
+  cudaStat = cudaMalloc((void**)&devPtrA, M*N * sizeof(*a));
+  // CHECK: if (cudaStat != hipSuccess) {
+  if (cudaStat != cudaSuccess) {
+    printf("device memory allocation failed");
+    return EXIT_FAILURE;
+  }
+  // CHECK: stat = rocblas_create_handle(&handle);
+  stat = cublasCreate(&handle);
+  // CHECK: if (stat != rocblas_status_success) {
+  if (stat != CUBLAS_STATUS_SUCCESS) {
+    printf("CUBLAS initialization failed\n");
+    return EXIT_FAILURE;
+  }
+  // CHECK: stat = rocblas_set_matrix(M, N, sizeof(*a), a, M, devPtrA, M);
+  stat = cublasSetMatrix(M, N, sizeof(*a), a, M, devPtrA, M);
+  // CHECK: if (stat != rocblas_status_success) {
+  if (stat != CUBLAS_STATUS_SUCCESS) {
+    printf("data download failed");
+    // CHECK: hipFree(devPtrA);
+    // CHECK: rocblas_destroy_handle(handle);
+    cudaFree(devPtrA);
+    cublasDestroy(handle);
+    return EXIT_FAILURE;
+  }
+  modify(handle, devPtrA, M, N, 2, 3, 16.0f, 12.0f);
+  // CHECK: stat = rocblas_get_matrix(M, N, sizeof(*a), devPtrA, M, a, M);
+  stat = cublasGetMatrix(M, N, sizeof(*a), devPtrA, M, a, M);
+  // CHECK: if (stat != rocblas_status_success) {
+  if (stat != CUBLAS_STATUS_SUCCESS) {
+    printf("data upload failed");
+    // CHECK: hipFree(devPtrA);
+    // CHECK: rocblas_destroy_handle(handle);
+    cudaFree(devPtrA);
+    cublasDestroy(handle);
+    return EXIT_FAILURE;
+  }
+  // CHECK: hipFree(devPtrA);
+  // CHECK: rocblas_destroy_handle(handle);
+  cudaFree(devPtrA);
+  cublasDestroy(handle);
+  for (j = 1; j <= N; j++) {
+    for (i = 1; i <= M; i++) {
+      printf("%7.0f", a[IDX2F(i, j, M)]);
+    }
+    printf("\n");
+  }
+  free(a);
+  return EXIT_SUCCESS;
+}
