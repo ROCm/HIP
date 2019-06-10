0480ff56f1441fc515d2c26ce33783e303423938
root <garg.rahul@gmail.com>
2019-06-10 22:12:33 +0000

    Add BUILD_CMD in hipOccupancyMaxPotentialBlockSize dtest

--- tests/src/runtimeApi/module/hipOccupancyMaxPotentialBlockSize.cpp
+++ tests/src/runtimeApi/module/hipOccupancyMaxPotentialBlockSize.cpp
@@ -22,6 +22,7 @@ THE SOFTWARE.
 // Test the Grid_Launch syntax.
 
 /* HIT_START
+ * BUILD_CMD: vcpy_kernel.code %hc --genco %S/vcpy_kernel.cpp -o vcpy_kernel.code
  * BUILD: %t %s ../../test_common.cpp NVCC_OPTIONS -std=c++11
  * TEST: %t
  * HIT_END
