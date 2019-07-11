3e213d94e181f0f1241b892e99dbf975a2866865
Jatin Chaudhary <51944368+cjatin@users.noreply.github.com>
2019-07-11 14:46:03 +0530

    Adding fix to initialize a dim3 variable from blockIdx. (#1212)
    Adding gitignore for ctags

--- include/hip/hcc_detail/hip_runtime_api.h
+++ include/hip/hcc_detail/hip_runtime_api.h
@@ -271,7 +271,7 @@ typedef struct dim3 {
     uint32_t y;  ///< y
     uint32_t z;  ///< z
 #ifdef __cplusplus
-    dim3(uint32_t _x = 1, uint32_t _y = 1, uint32_t _z = 1) : x(_x), y(_y), z(_z){};
+    __host__ __device__ dim3(uint32_t _x = 1, uint32_t _y = 1, uint32_t _z = 1) : x(_x), y(_y), z(_z){};
 #endif
 } dim3;
 
