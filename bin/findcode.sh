dabf20cb0bfd51d737f649620830f43774c736ea
Evgeny Mankov <evgeny.mankov@gmail.com>
2019-07-17 10:28:54 +0300

    [HIPIFY][bash] Make 'find' sorted by extension (#1231)
    + Source files are the first to go. It is needed for in-place hipification in order to avoid errors with included but already hipified header files.
    + More extensions support for batch processing.
    
    [ROCm/hip commit: b1fad7915b184f0cc6fa8914104c8125765aef9a]

--- bin/findcode.sh
+++ bin/findcode.sh
@@ -2,4 +2,7 @@
 
 SEARCH_DIRS=$@
 
-find $SEARCH_DIRS -name '*.cpp' -o -name '*.h' -o -name '*.cu' -o -name '*.cuh' -o -name '*.c' -o -name '*.hpp' -o -name '*.inl'
+find $SEARCH_DIRS -name '*.cu'
+find $SEARCH_DIRS -name '*.cpp' -o -name '*.cxx' -o -name '*.c' -o -name '*.cc'
+find $SEARCH_DIRS -name '*.cuh'
+find $SEARCH_DIRS -name '*.h' -o -name '*.hpp' -o -name '*.inc' -o -name '*.inl' -o -name '*.hxx' -o -name '*.hdl'
