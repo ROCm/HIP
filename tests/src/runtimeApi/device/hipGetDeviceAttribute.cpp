609496305848ef31c06db54d2b1358215a31ae1c
Wilkin <Wing-Ki.ChauWilkin@amd.com>
2019-07-19 10:27:35 -0400

    Added NVCC blocker for the hip*FlushCntl test cases

--- tests/src/runtimeApi/device/hipGetDeviceAttribute.cpp
+++ tests/src/runtimeApi/device/hipGetDeviceAttribute.cpp
@@ -132,7 +132,9 @@ int main(int argc, char* argv[]) {
     CHECK(test_hipDeviceGetAttribute(deviceId, hipDeviceAttributeMaxTexture3DWidth, props.maxTexture3D[0]));
     CHECK(test_hipDeviceGetAttribute(deviceId, hipDeviceAttributeMaxTexture3DHeight, props.maxTexture3D[1]));
     CHECK(test_hipDeviceGetAttribute(deviceId, hipDeviceAttributeMaxTexture3DDepth, props.maxTexture3D[2]));
+#ifndef __HIP_PLATFORM_NVCC__
     CHECK(test_hipDeviceGetHdpAddress(deviceId, hipDeviceAttributeHdpMemFlushCntl, props.hdpMemFlushCntl));
     CHECK(test_hipDeviceGetHdpAddress(deviceId, hipDeviceAttributeHdpRegFlushCntl, props.hdpRegFlushCntl));
+#endif
     passed();
 };
