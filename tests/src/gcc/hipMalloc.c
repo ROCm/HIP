a6ac40b4ab6059359ab99c59758fc137c2eebf75
Sarbojit Sarkar <ssarkar@hcBLR-Fiji3.amd.com>
2019-07-25 21:33:57 +0530

    Added relative path for library

--- tests/src/gcc/hipMalloc.c
+++ tests/src/gcc/hipMalloc.c
@@ -18,7 +18,7 @@
  * */
 
 /* HIT_START
- * BUILD_CMD: hipMalloc %cc -D__HIP_PLATFORM_HCC__ -I%hip-path/include %S/%s -L%hip-path/lib -l:libhip_hcc.so -o %T/%t EXCLUDE_HIP_PLATFORM nvcc
+ * BUILD_CMD: hipMalloc %cc -D__HIP_PLATFORM_HCC__ -I%hip-path/include %S/%s -Wl,--rpath=%hip-path/lib %hip-path/lib/libhip_hcc.so -o %T/%t EXCLUDE_HIP_PLATFORM nvcc
  * TEST: %t EXCLUDE_HIP_PLATFORM nvcc
  * HIT_END
  */
