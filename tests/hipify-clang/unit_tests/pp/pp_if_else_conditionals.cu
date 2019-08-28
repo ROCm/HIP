24be21495d05b3adf82cd7263c5d4f42ed523c50
Evgeny Mankov <evgeny.mankov@gmail.com>
2019-08-28 21:17:35 +0300

    [HIPIFY][#207][fix] Translate all preprocessor's conditional blocks
    + Start to translate preprocessor's false conditional blocks too:
      based on clang's https://reviews.llvm.org/D66597;
      available only starting from LLVM 10.0 or trunk.
    + Option -skip-excluded-preprocessor-conditional-blocks for skipping excluded conditional blocks:
      the default behavior for hipify-clang built with LLVM < 10.0;
      false by default for hipify-clang built with LLVM 10 or trunk.
    + Add 4 preprocessor unit tests, 2 of which are LLVM 10.0 only
    + Update couple of existing tests by setting -skip-excluded-preprocessor-conditional-blocks option:
      update lit testing accordingly

---
+++ tests/hipify-clang/unit_tests/pp/pp_if_else_conditionals.cu
@@ -0,0 +1,30 @@
+// RUN: %run_test hipify "%s" "%t" %hipify_args "--skip-excluded-preprocessor-conditional-blocks" %clang_args
+// CHECK: #include <hip/hip_runtime.h>
+
+#include <cuda.h>
+
+__global__ void axpy_kernel(float a, float* x, float* y) {
+  y[threadIdx.x] = a * x[threadIdx.x];
+}
+
+void axpy(float a, float* x, float* y) {
+
+#ifdef SOME_MACRO
+  // CHECK: axpy_kernel <<<1, 1>>> (a, y, x);
+  axpy_kernel <<<1, 1>>> (a, y, x);
+#endif
+
+#ifndef SOME_MACRO
+  // CHECK: hipLaunchKernelGGL(axpy_kernel, dim3(1), dim3(2), 0, 0, a, y, x);
+  axpy_kernel <<<1, 2>>> (a, y, x);
+#endif
+
+#ifdef SOME_MACRO
+  // CHECK: axpy_kernel <<<1, 3>>> (a, y, x);
+  axpy_kernel <<<1, 3>>> (a, y, x);
+#else
+  // CHECK: hipLaunchKernelGGL(axpy_kernel, dim3(1), dim3(4), 0, 0, a, x, y);
+  axpy_kernel <<<1, 4>>> (a, x, y);
+#endif
+
+}
