65420fa17a6229ab69cec7c7bf88c112bd51596f
Alex Voicu <alexandru.voicu@amd.com>
2019-11-02 22:02:08 +0200

    Separate volatile for clarity. Handle assignment.
    [ROCm/hip commit: 99b9d5449fcfc5e4ab1d23c2992dd7f4187e9127]

--- include/hip/hcc_detail/hip_vector_types.h
+++ include/hip/hcc_detail/hip_vector_types.h
@@ -54,8 +54,17 @@ THE SOFTWARE.
                 const Scalar_accessor* p;
 
                 __host__ __device__
+                operator const T*() const noexcept {
+                    return &reinterpret_cast<const T*>(p)[idx];
+                }
+                __host__ __device__
                 operator const T*() const volatile noexcept {
                     return &reinterpret_cast<const T*>(p)[idx];
+                }
+                __host__ __device__
+                operator T*() noexcept {
+                    return &reinterpret_cast<T*>(
+                        const_cast<Scalar_accessor*>(p))[idx];
                 }
                 __host__ __device__
                 operator T*() volatile noexcept {
@@ -68,6 +77,8 @@ THE SOFTWARE.
             Vector data;
 
             __host__ __device__
+            operator T() const noexcept { return data[idx]; }
+            __host__ __device__
             operator T() const volatile noexcept { return data[idx]; }
 
             __host__ __device__
@@ -75,6 +86,12 @@ THE SOFTWARE.
 
             __host__ __device__
             Scalar_accessor& operator=(T x) noexcept {
+                data[idx] = x;
+
+                return *this;
+            }
+            __host__ __device__
+            volatile Scalar_accessor& operator=(T x) volatile noexcept {
                 data[idx] = x;
 
                 return *this;
