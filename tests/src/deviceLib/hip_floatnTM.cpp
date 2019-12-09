e19784bfbb5443292ab24af2215bd590fb3b555a
Jatin Chaudhary <jachaudh@amd.com>
2019-12-09 11:48:59 +0530

    Adding changes in test

--- tests/src/deviceLib/hip_floatnTM.cpp
+++ tests/src/deviceLib/hip_floatnTM.cpp
@@ -33,88 +33,74 @@ THE SOFTWARE.
 
 
 template <typename T, typename M>
-int count(T& a) {
-    return sizeof(a) / sizeof(T);
-}
-/*
-template <typename T>
-void cpuJitter(T& b) {}
-
-template <>
-void cpuJitter<float2>(float2& b) {
-   b.x++;
-   b.y++;
-   b.x += b.y;
+int count() {
+    return sizeof(T) / sizeof(M);
 }
 
-template <>
-void cpuJitter<float3>(float3& b) {
-   cpuJitter<float2>(*reinterpret_cast<float2*>(&b));
-   b.z++;
-   b.x = b.y + b.z;
-}
-
-template <>
-void cpuJitter<float4>(float4& b) {
-   cpuJitter<float2>(*reinterpret_cast<float2*>(&b));
-   b.w++;
-   b.x = b.w + b.y + b.z;
-}
-*/
-
-template <typename T, size_t N = 2>
-void cpuJitter(T& b) {
+template <template N>
+void cpuJitter2(N& b) {
     b.x++;
     b.y++;
     b.x += b.y;
 }
 
-template <typename T, size_t N = 3>
-void cpuJitter(T& b) {
-    cpuJitter<T, N - 1>(*reinterpret_cast<T*>(&b));
+template <template M, template N>
+void cpuJitter3(M& b) {
+    cpuJitter2<N>(*reinterpret_cast<N*>(&b));
     b.z++;
     b.x = b.y + b.z;
 }
 
-template <typename T, size_t N = 4>
-void cpuJitter(T& b) {
-    cpuJitter<T, N - 1>(*reinterpret_cast<T*>(&b));
+template <typename T, template M, template N>
+void cpuJitter4(T& b) {
+    cpuJitter3<M, N>(*reinterpret_cast<M*>(&b));
     b.w++;
     b.x = b.w + b.y + b.z;
 }
 
 // Rotate x,y,z,w by 1
-template <typename T>
-void cpuRotate(T& a, T& b) {}
-
-template <>
-void cpuRotate<float2>(float2& a, float2& b) {
+template <template N>
+void cpuRotate2(N& a, N& b) {
     b.x = a.y;
     b.y = a.x;
-    cpuJitter<float2, 2>(b);
+    cpuJitter2<N>(b);
 }
 
-template <>
-void cpuRotate<float3>(float3& a, float3& b) {
-    cpuRotate<float2>(*reinterpret_cast<float2*>(&a), *reinterpret_cast<float2*>(&b));
+template <template M, template N>
+void cpuRotate3(M& a, M& b) {
+    cpuRotate2<N>(*reinterpret_cast<N*>(&a), *reinterpret_cast<N*>(&b));
     b.y = a.z;
     b.z = a.x;
-    cpuJitter<float3, 3>(b);
+    cpuJitter3<N>(b);
 }
 
-template <>
-void cpuRotate<float4>(float4& a, float4& b) {
-    cpuRotate<float2>(*reinterpret_cast<float2*>(&a), *reinterpret_cast<float2*>(&b));
+template <typename T, template M, template N>
+void cpuRotate4(T& a, T& b) {
+    cpuRotate3<M, N>(*reinterpret_cast<M*>(&a), *reinterpret_cast<M*>(&b));
     b.y = a.z;
     b.z = a.w;
     b.w = a.x;
-    cpuJitter<float4, 4>(b);
+    cpuJitter4<M, N>(b);
 }
 
 template <typename T>
-void cpuRotate(T* a, T* b, int size) {
+void cpuRotate2(T* a, T* b, int size) {
     for (int i = 0; i < size; i++) {
-        cpuRotate(a[i], b[i]);
+        cpuRotate2<T>(a[i], b[i]);
+    }
+}
+
+template <typename T, typename M>
+void cpuRotate3(T* a, T* b, int size) {
+    for (int i = 0; i < size; i++) {
+        cpuRotate<T, M>(a[i], b[i]);
+    }
+}
+
+template <typename T, template M, template N>
+void cpuRotate4(T* a, T* b, int size) {
+    for (int i = 0; i < size; i++) {
+        cpuRotate4<T, M, N>(a[i], b[i]);
     }
 }
 
@@ -128,22 +114,19 @@ inline int getRandomNumber(int min = INT_MIN, int max = INT_MAX) {
 inline float getRandomFloat() { return getRandomNumber() / getRandomNumber(); }
 
 template <typename T>
-void fillMatrix(T* a, int size) {}
-
-template <>
-void fillMatrix<float2>(float2* a, int size) {
+void fillMatrix2(T* a, int size) {
     for (int i = 0; i < size; i++) {
-        float2 t;
+        T t;
         t.x = getRandomFloat();
         t.y = getRandomFloat();
         a[i] = t;
     }
 }
 
-template <>
-void fillMatrix<float3>(float3* a, int size) {
+template <typename T>
+void fillMatrix3(T* a, int size) {
     for (int i = 0; i < size; i++) {
-        float3 t;
+        T t;
         t.x = getRandomFloat();
         t.y = getRandomFloat();
         t.z = getRandomFloat();
@@ -151,10 +134,10 @@ void fillMatrix<float3>(float3* a, int size) {
     }
 }
 
-template <>
-void fillMatrix<float4>(float4* a, int size) {
+template <typename T>
+void fillMatrix4(T* a, int size) {
     for (int i = 0; i < size; i++) {
-        float4 t;
+        T t;
         t.x = getRandomFloat();
         t.y = getRandomFloat();
         t.z = getRandomFloat();
@@ -187,6 +170,7 @@ bool isEqual(T* a, T* b, int size) {
     }
     return true;
 }
+
 template <typename T>
 __global__ void gMatAcc(T* a, T* b, int size) {
     int i = threadIdx.x;
@@ -195,111 +179,56 @@ __global__ void gMatAcc(T* a, T* b, int size) {
     a[i] += (a[i] * b[i]);
 }
 
+template <typename T, typename S, typename A, typename B, typename C>
+bool testType(int msize) {
+    T *fa, *fb, *fc;
+    fa = new T[msize];
+    fb = new T[msize];
+    fc = new T[msize];
+
+    T *d_fa, *d_fb;
+
+    int c = count<T, S>();
+    if (c == 4) {
+        fillMatrix4<T>(fa, msize);
+        cpuRotate4<A, B, C>(fa, fb, msize);
+    } else if (c == 3) {
+        fillMatrix3<T>(fa, msize);
+        cpuRotate3<B, C>(fa, fb, msize);
+    } else if (c == 2) {
+        fillMatrix2<T>(fa, msize);
+        cpuRotate2<C>(fa, fb, msize);
+    } else {
+        failed("Invalid Size\n");
+    }
+
+    hipMalloc(&d_fa, sizeof(T) * msize);
+    hipMalloc(&d_fb, sizeof(T) * msize);
+
+    hipMemcpy(d_fa, fa, sizeof(T) * msize, hipMemcpyHostToDevice);
+    hipMemcpy(d_fb, fb, sizeof(T) * msize, hipMemcpyHostToDevice);
+
+    matAcc<T>(fa, fb, msize);
+
+    hipLaunchKernelGGL(gMatAcc, 1, msize, 0, 0, d_fa, d_fb, msize);
+
+    hipMemcpy(fc, d_fa, sizeof(T) * msize, hipMemcpyDeviceToHost);
+
+    delete[] fa;
+    delete[] fb;
+    delete[] fc;
+    hipFree(d_fa);
+    hipFree(d_fb);
+
+    if (!isEqual<T>(fa, fc, msize)) {
+        failed("Failed for:: ");
+        failed(typeid(T).name().c_str());
+    }
+    return true;
+}
+
 int main() {
     const int msize = 500;
-    {
-        float2 *fa, *fb, *fc;
-        fa = new float2[msize];
-        fb = new float2[msize];
-        fc = new float2[msize];
-
-        float2 *d_fa, *d_fb;
-
-        fillMatrix<float2>(fa, msize);
-
-        cpuRotate<float2>(fa, fb, msize);
-
-        hipMalloc(&d_fa, sizeof(float2) * msize);
-        hipMalloc(&d_fb, sizeof(float2) * msize);
-
-        hipMemcpy(d_fa, fa, sizeof(float2) * msize, hipMemcpyHostToDevice);
-        hipMemcpy(d_fb, fb, sizeof(float2) * msize, hipMemcpyHostToDevice);
-
-        matAcc<float2>(fa, fb, msize);
-
-        hipLaunchKernelGGL(gMatAcc, 1, msize, 0, 0, d_fa, d_fb, msize);
-
-        hipMemcpy(fc, d_fa, sizeof(float2) * msize, hipMemcpyDeviceToHost);
-
-        if (!isEqual<float2>(fa, fc, msize)) {
-            failed("Fail float2");
-        }
-
-        delete[] fa;
-        delete[] fb;
-        delete[] fc;
-        hipFree(d_fa);
-        hipFree(d_fb);
-    }
-
-    {
-        float3 *fa, *fb, *fc;
-        fa = new float3[msize];
-        fb = new float3[msize];
-        fc = new float3[msize];
-
-        float3 *d_fa, *d_fb;
-
-        fillMatrix<float3>(fa, msize);
-
-        cpuRotate<float3>(fa, fb, msize);
-
-        hipMalloc(&d_fa, sizeof(float3) * msize);
-        hipMalloc(&d_fb, sizeof(float3) * msize);
-
-        hipMemcpy(d_fa, fa, sizeof(float3) * msize, hipMemcpyHostToDevice);
-        hipMemcpy(d_fb, fb, sizeof(float3) * msize, hipMemcpyHostToDevice);
-
-        matAcc<float3>(fa, fb, msize);
-
-        hipLaunchKernelGGL(gMatAcc, 1, msize, 0, 0, d_fa, d_fb, msize);
-
-        hipMemcpy(fc, d_fa, sizeof(float3) * msize, hipMemcpyDeviceToHost);
-
-        if (!isEqual<float3>(fa, fc, msize)) {
-            failed("Fail float3");
-        }
-
-        delete[] fa;
-        delete[] fb;
-        delete[] fc;
-        hipFree(d_fa);
-        hipFree(d_fb);
-    }
-
-    {
-        float4 *fa, *fb, *fc;
-        fa = new float4[msize];
-        fb = new float4[msize];
-        fc = new float4[msize];
-
-        float4 *d_fa, *d_fb;
-
-        fillMatrix<float4>(fa, msize);
-
-        cpuRotate<float4>(fa, fb, msize);
-
-        hipMalloc(&d_fa, sizeof(float4) * msize);
-        hipMalloc(&d_fb, sizeof(float4) * msize);
-
-        hipMemcpy(d_fa, fa, sizeof(float4) * msize, hipMemcpyHostToDevice);
-        hipMemcpy(d_fb, fb, sizeof(float4) * msize, hipMemcpyHostToDevice);
-
-        matAcc<float4>(fa, fb, msize);
-
-        hipLaunchKernelGGL(gMatAcc, 1, msize, 0, 0, d_fa, d_fb, msize);
-
-        hipMemcpy(fc, d_fa, sizeof(float4) * msize, hipMemcpyDeviceToHost);
-
-        if (!isEqual<float4>(fa, fc, msize)) {
-            failed("Fail float4");
-        }
-
-        delete[] fa;
-        delete[] fb;
-        delete[] fc;
-        hipFree(d_fa);
-        hipFree(d_fb);
-    }
+    testType<float2, float, float4, float3, float2>();
     passed();
 }
