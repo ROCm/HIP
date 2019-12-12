9b6f6d7c5178a9d8bd4ebfdef57e7d5e5d2cb10d
Vladislav Sytchenko <vsytchen@amd.com>
2019-12-11 20:21:12 -0500

    Reduce the amount of free host memory to 40% of what is reported on Windows, otherwise we can run into OOM situations.
    [ROCm/hip commit: b12c53ccebb6c13fcc89426428ef85bfe4929a0c]

--- tests/src/runtimeApi/memory/hipMemcpy.cpp
+++ tests/src/runtimeApi/memory/hipMemcpy.cpp
@@ -292,8 +292,8 @@ void memcpytest2_get_host_memory(size_t& free, size_t& total) {
     MEMORYSTATUSEX status;
     status.dwLength = sizeof(status);
     GlobalMemoryStatusEx(&status);
-    free = status.ullAvailPhys;
-    total = status.ullTotalPhys;
+    free = (0.4 * status.ullAvailPhys);
+    total = (0.4 * status.ullTotalPhys);
 }
 #else
 struct sysinfo memInfo;
