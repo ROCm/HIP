f4d79f2a47b0e7e5b603735e70c7de7c269a7f93
Laurent Morichetti <laurent.morichetti@amd.com>
2020-01-31 06:58:10 -0800

    Enable multi platform excludes
    Replaced _exclude_platforms' string with a list. Use IN_LIST to check
    if a platform is excluded.

--- tests/src/kernel/hipDynamicShared.cpp
+++ tests/src/kernel/hipDynamicShared.cpp
@@ -21,7 +21,7 @@ THE SOFTWARE.
 */
 
 /* HIT_START
- * BUILD: %t %s ../test_common.cpp EXCLUDE_HIP_PLATFORM hcc
+ * BUILD: %t %s ../test_common.cpp EXCLUDE_HIP_PLATFORM hcc vdi
  * TEST: %t EXCLUDE_HIP_PLATFORM nvcc
  * HIT_END
  */
