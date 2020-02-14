cfe399ce035ad6970e0d4c68b996218b26ff3f6a
Payam <payam.ghafari@amd.com>
2020-02-13 21:37:32 -0500

    removing oocupancy tests from hip-vdi for now
    Change-Id: I06e966d07021af87fa2c195e4bbaf6bcc336658d
    
    
    [ROCm/hip commit: fabe62e31f5c9b7cbf1c1cad70177e108758d72e]

--- tests/src/runtimeApi/module/hipOccupancyMaxPotentialBlockSize.cpp
+++ tests/src/runtimeApi/module/hipOccupancyMaxPotentialBlockSize.cpp
@@ -22,7 +22,7 @@ THE SOFTWARE.
 // Test the Grid_Launch syntax.
 
 /* HIT_START
- * BUILD: %t %s ../../test_common.cpp EXCLUDE_HIP_PLATFORM nvcc
+ * BUILD: %t %s ../../test_common.cpp EXCLUDE_HIP_PLATFORM nvcc vdi
  * TEST: %t
  * HIT_END
  */
