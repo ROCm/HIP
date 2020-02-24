34806c332bbd8c2e5c7bc9e30d4651ce1c76d56a
Payam <payam.ghafari@amd.com>
2020-02-24 17:37:21 -0500

    updated hip-targets for hip-vdi
    Change-Id: Ib65e9011b25e368b8e03db1b4f14aa1ad076f4bc
    
    
    [ROCm/hip commit: 73686f990d7a92ce69cce392fda5d6892bb8aebc]

--- packaging/hip-targets-release.cmake
+++ packaging/hip-targets-release.cmake
@@ -32,20 +32,28 @@ endif()
 # Import target "hip::hip_hcc" for configuration "Release"
 set_property(TARGET hip::hip_hcc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 if(HIP_COMPILER STREQUAL "clang")
-set_target_properties(hip::hip_hcc PROPERTIES
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libhip_hcc.so"
-  IMPORTED_SONAME_RELEASE "libhip_hcc.so"
-  )
+  if(HIP_RUNTIME STREQUAL "vdi")
+    set_target_properties(hip::amdhip64 PROPERTIES
+      IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libamdhip64.so"
+      IMPORTED_SONAME_RELEASE "libamdhip64.so")
+  else()
+    set_target_properties(hip::hip_hcc PROPERTIES
+      IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libhip_hcc.so"
+      IMPORTED_SONAME_RELEASE "libhip_hcc.so")
+  endif()
 else()
-set_target_properties(hip::hip_hcc PROPERTIES
-  IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "hcc::hccrt;hcc::hc_am"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libhip_hcc.so"
-  IMPORTED_SONAME_RELEASE "libhip_hcc.so"
-  )
+  set_target_properties(hip::hip_hcc PROPERTIES
+    IMPORTED_LINK_INTERFACE_LIBRARIES_RELEASE "hcc::hccrt;hcc::hc_am"
+    IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libhip_hcc.so"
+    IMPORTED_SONAME_RELEASE "libhip_hcc.so")
 endif()
 
-list(APPEND _IMPORT_CHECK_TARGETS hip::hip_hcc )
-list(APPEND _IMPORT_CHECK_FILES_FOR_hip::hip_hcc "${_IMPORT_PREFIX}/lib/libhip_hcc.so" )
-
+if(NOT HIP_RUNTIME STREQUAL "vdi")
+  list(APPEND _IMPORT_CHECK_TARGETS hip::hip_hcc )
+  list(APPEND _IMPORT_CHECK_FILES_FOR_hip::hip_hcc "${_IMPORT_PREFIX}/lib/libhip_hcc.so" )
+else()
+  list(APPEND _IMPORT_CHECK_TARGETS hip::amdhip64)
+  list(APPEND _IMPORT_CHECK_FILES_FOR_hip::amdhip64 "${_IMPORT_PREFIX}/lib/libamdhip64.so" )
+endif()
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
