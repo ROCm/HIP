23be4c544b96654331858c92620762e33b17bc74
root <garg.rahul@gmail.com>
2020-03-19 02:27:02 +0000

    Fix HIT issue

--- tests/src/runtimeApi/module/hipModuleOccupancyMaxPotentialActiveBlockSize.cpp
+++ tests/src/runtimeApi/module/hipModuleOccupancyMaxPotentialActiveBlockSize.cpp
@@ -18,7 +18,6 @@ THE SOFTWARE.
 */
 
 /* HIT_START
- * BUILD_CMD: vcpy_kernel.code %hc --genco %S/vcpy_kernel.cpp -o vcpy_kernel.code
  * BUILD: %t %s ../../test_common.cpp NVCC_OPTIONS -std=c++11
  * TEST: %t
  * HIT_END
