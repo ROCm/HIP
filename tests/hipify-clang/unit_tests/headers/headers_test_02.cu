01058bb2c58745ff15dab411a944182849e44c99
Evgeny Mankov <evgeny.mankov@gmail.com>
2020-04-07 11:33:30 +0300

    [HIP][HIPIFY] Remove HIPIFY related stuff due to moving of HIPIFY to a separate repo (#2002)
    HIPIFY tools now are here: https://github.com/ROCm-Developer-Tools/HIPIFY
    
    [ROCm/hip commit: 5f1420a229b41662f1f3f178de56daa10028d39c]

--- tests/hipify-clang/unit_tests/headers/headers_test_02.cu
+++
@@ -1,8 +0,0 @@
-// RUN: %run_test hipify "%s" "%t" %hipify_args %clang_args
-
-// CHECK: #include "hip/hip_runtime.h"
-// CHECK-NOT: #include "cuda_runtime.h"
-// CHECK: #include <stdio.h>
-#include "cuda.h"
-#include "cuda_runtime.h"
-#include <stdio.h>
--- tests/hipify-clang/unit_tests/libraries/CAFFE2/caffe2/core/common_cudnn.h
+++
@@ -1,7 +0,0 @@
-#ifndef CAFFE2_CORE_COMMON_CUDNN_H_
-#define CAFFE2_CORE_COMMON_CUDNN_H_
-
-#include <array>
-#include <mutex>
-
-#endif  // CAFFE2_CORE_COMMON_CUDNN_H_
