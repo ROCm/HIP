5f1420a229b41662f1f3f178de56daa10028d39c
Evgeny Mankov <evgeny.mankov@gmail.com>
2020-04-07 11:33:30 +0300

    [HIP][HIPIFY] Remove HIPIFY related stuff due to moving of HIPIFY to a separate repo (#2002)
    HIPIFY tools now are here: https://github.com/ROCm-Developer-Tools/HIPIFY

--- tests/hipify-clang/unit_tests/headers/headers_test_06.cu
+++
@@ -1,8 +0,0 @@
-// RUN: %run_test hipify "%s" "%t" %hipify_args %clang_args
-
-// CHECK: #include <hipblas.h>
-// CHECK-NOT: #include <cublas_v2.h>
-// CHECK: #include <stdio.h>
-#include <cublas.h>
-#include <cublas_v2.h>
-#include <stdio.h>
--- tests/hipify-clang/unit_tests/headers/headers_test_07.cu
+++
@@ -1,8 +0,0 @@
-// RUN: %run_test hipify "%s" "%t" %hipify_args %clang_args
-
-// CHECK: #include "hipblas.h"
-// CHECK-NOT: #include "cublas.h"
-// CHECK: #include <stdio.h>
-#include "cublas_v2.h"
-#include "cublas.h"
-#include <stdio.h>
