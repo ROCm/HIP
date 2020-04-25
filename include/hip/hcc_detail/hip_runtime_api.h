de20ec1da255ca7dab57e4864397626eb88fe046
Rahul Garg <rahul.garg@amd.com>
2020-04-25 06:42:21 +0530

    Fix channelFormatDescriptor on HIP/HCC

--- include/hip/hcc_detail/hip_runtime_api.h
+++ include/hip/hcc_detail/hip_runtime_api.h
@@ -3937,7 +3937,7 @@ static inline hipError_t hipBindTextureToArray(
     const struct texture<T, dim, readMode> &tex,
     hipArray_const_t array)
 {
-    struct cudaChannelFormatDesc desc;
+    struct hipChannelFormatDesc desc;
     hipError_t err = hipGetChannelDesc(&desc, array);
     return (err == hipSuccess) ? hipBindTextureToArray(tex, array, desc) : err;
 }
@@ -3970,7 +3970,7 @@ template<class T, int dim, enum hipTextureReadMode readMode>
 static inline hipError_t hipBindTextureToMipmappedArray(
     const struct texture<T, dim, readMode> &tex,
     hipMipmappedArray_const_t mipmappedArray,
-    const struct cudaChannelFormatDesc &desc)
+    const struct hipChannelFormatDesc &desc)
 {
     return hipBindTextureToMipmappedArray(&tex, mipmappedArray, &desc);
 }
