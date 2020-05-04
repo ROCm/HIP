6e972dd3bb94631541805b0795f286a9cf0c1339
Tao Sang <tao.sang@amd.com>
2020-05-04 18:06:34 -0400

    Support performance tests
    Support performance tests while direct tests commands keep unchanged.
    To build performance tests, run "make build_perf".
    To run all performance testis, run "make perf".
    To run specific tests, for example, run
    /usr/bin/ctest -C performance -R performance_tests/perfDispatch --verbose
    To run individual test, for example, run
    performance_tests/memory/hipPerfMemMallocCpyFree
    
    Change-Id: I168c1b9ef1ec21b392d48648d0c71e8fbd37d57b

--- tests/performance/perfDispatch/hipPerfDispatchSpeed.cpp
+++ tests/performance/perfDispatch/hipPerfDispatchSpeed.cpp
@@ -7,7 +7,7 @@
 #include "test_common.h"
 
 /* HIT_START
- * BUILD: %t %s ../../test_common.cpp timer.cpp EXCLUDE_HIP_PLATFORM nvcc
+ * BUILD: %t %s ../../src/test_common.cpp timer.cpp EXCLUDE_HIP_PLATFORM nvcc
  * TEST: %t
  * HIT_END
  */
