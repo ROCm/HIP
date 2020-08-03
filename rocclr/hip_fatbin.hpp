2813f79fc51a2ff1edeb93f84212edca621c4ba5
kjayapra-amd <karthik.jayaprakash@amd.com>
2020-08-03 11:23:33 -0400

    SWDEV-241902 - Changes to pass file descriptor and offset to load code object.
    Change-Id: I96add27f4fde1b5ee5501e206b7e85ab77e87dfc
    
    
    [ROCm/hip commit: 89e5e8b90c419226395ba7ee6cb0b1a0f5f81717]

--- rocclr/hip_fatbin.hpp
+++ rocclr/hip_fatbin.hpp
@@ -1,28 +1,86 @@
 #ifndef HIP_FAT_BINARY_HPP
 #define HIP_FAT_BINARY_HPP
 
+#include "hip/hip_runtime.h"
+#include "hip/hip_runtime_api.h"
+#include "hip_internal.hpp"
+#include "platform/program.hpp"
+
 namespace hip {
 
-class FatBinaryMetaInfo {
+//Fat Binary Per Device info
+class FatBinaryDeviceInfo {
 public:
-  FatBinaryMetaInfo(bool built, const void* binary_ptr, size_t binary_size):
-                    built_(built), binary_ptr_(binary_ptr), binary_size_(binary_size) {}
-  ~FatBinaryMetaInfo() {}
+  FatBinaryDeviceInfo (const void* binary_image, size_t binary_size, size_t binary_offset)
+                      : binary_image_(binary_image), binary_size_(binary_size),
+                        binary_offset_(binary_offset), program_(nullptr),
+                        add_dev_prog_(false), prog_built_(false) {}
 
-  //Set once the mod has been built
-  void set_built() { built_ = true; }
+  ~FatBinaryDeviceInfo();
 
-  //Accessor for private vars
-  bool built() const { return built_; }
-  const void* binary_ptr() const { return binary_ptr_; }
-  size_t binary_size() const { return binary_size_; }
 private:
-  bool built_;              //Set when mod is built. Used in Lazy Binary
-  const void* binary_ptr_;  //Binary image ptr
-  size_t binary_size_;      //Binary Size
+  const void* binary_image_; // binary image ptr
+  size_t binary_size_;       // binary image size
+  size_t binary_offset_;     // image offset from original
+
+  amd::Program* program_;    // reinterpreted as hipModule_t
+  friend class FatBinaryInfo;
+
+  //Control Variables
+  bool add_dev_prog_;
+  bool prog_built_;
 };
 
-typedef std::vector<std::pair<hipModule_t, FatBinaryMetaInfo*>> FatBinaryInfoType;
+
+// Fat Binary Info
+class FatBinaryInfo {
+public:
+  FatBinaryInfo(const char* fname, const void* image);
+  ~FatBinaryInfo();
+
+  // Loads Fat binary from file or image, unbundles COs for devices.
+  hipError_t ExtractFatBinary(const std::vector<hip::Device*>& devices);
+  hipError_t AddDevProgram(const int device_id);
+  hipError_t BuildProgram(const int device_id);
+
+
+  // Device Id bounds check
+  inline void DeviceIdCheck(const int device_id) const {
+    guarantee(device_id >= 0);
+    guarantee(static_cast<size_t>(device_id) < fatbin_dev_info_.size());
+  }
+
+  // Getter Methods
+  amd::Program* GetProgram(int device_id) {
+    DeviceIdCheck(device_id);
+    return fatbin_dev_info_[device_id]->program_;
+  }
+
+  hipModule_t Module(int device_id) const {
+    DeviceIdCheck(device_id);
+    return reinterpret_cast<hipModule_t>(as_cl(fatbin_dev_info_[device_id]->program_));
+  }
+
+  hipError_t GetModule(int device_id, hipModule_t* hmod) const {
+    DeviceIdCheck(device_id);
+    *hmod = reinterpret_cast<hipModule_t>(as_cl(fatbin_dev_info_[device_id]->program_));
+    return hipSuccess;
+  }
+
+private:
+  std::string fname_;        // File name
+  amd::Os::FileDesc fdesc_;  // File descriptor
+  size_t fsize_;             // Total file size
+
+  // Even when file is passed image will be mmapped till ~desctructor.
+  const void* image_;        // Image
+
+  // Only used for FBs where image is directly passed
+  std::string uri_;          // Uniform resource indicator
+
+  // Per Device Info, like corresponding binary ptr, size.
+  std::vector<FatBinaryDeviceInfo*> fatbin_dev_info_;
+};
 
 }; /* namespace hip */
 
