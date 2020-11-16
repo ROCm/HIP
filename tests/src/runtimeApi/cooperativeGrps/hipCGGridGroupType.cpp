6f850bc2954d941389f160c8d07e5236957df5a7
Michael LIAO <michaelhongbin.liao@amd.com>
2020-11-16 12:35:20 -0500

    Fix cooperative group tests.
    - `gm` should be a global variable instead of auto local variable.
    
    Change-Id: Ibde1e54ee83fa5725f21820b4b792a97bf7df6ad
    
    
    [ROCm/hip commit: 6bf6480283a2826e2912048d7289033ab1e0572c]

--- tests/src/runtimeApi/cooperativeGrps/hipCGGridGroupType.cpp
+++ tests/src/runtimeApi/cooperativeGrps/hipCGGridGroupType.cpp
@@ -36,6 +36,8 @@ THE SOFTWARE.
 
 using namespace cooperative_groups;
 
+static __device__ int gm[2];
+
 static __global__
 void kernel_cg_grid_group_type(int *sizeTestD,
                                int *thdRankTestD,
@@ -55,7 +57,6 @@ void kernel_cg_grid_group_type(int *sizeTestD,
   isValidTestD[gIdx] = gg.is_valid();
 
   // Test sync
-  __device__ int gm[2];
   if (blockIdx.x == 0 && threadIdx.x == 0)
     gm[0] = 10;
   else if (blockIdx.x == 1 && threadIdx.x == 0)
