337369fec8b1beb59d43c1c325e9524d24f6a1e6
Sourabh Betigeri <sourabh.betigeri@amd.com>
2021-03-19 13:31:53 -0700

    SWDEV-274727 - Disabling tests temporarily
    Change-Id: I695daebe15f592afdfee8e4efe4d8966b6e9922a
    
    
    [ROCm/hip commit: 8f67e24cbd820c2ac6ce11f0469d3e5ec6f8d5e2]

--- tests/src/runtimeApi/event/hipEventIpc.cpp
+++ tests/src/runtimeApi/event/hipEventIpc.cpp
@@ -24,7 +24,7 @@ THE SOFTWARE.
 // forces synchronization : set
 
 /* HIT_START
- * BUILD: %t %s ../../test_common.cpp EXCLUDE_HIP_PLATFORM nvidia
+ * BUILD: %t %s ../../test_common.cpp EXCLUDE_HIP_PLATFORM all
  * TEST: %t --iterations 10
  * HIT_END
  */
