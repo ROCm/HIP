4de1bfafccb20b57b9e74eea11d2d7575dac8b23
Anusha GodavarthySurya <Anusha.GodavarthySurya@amd.com>
2021-04-19 11:25:05 -0700

    SWDEV-282361 - HIP support for NVRTC
    Change-Id: I82869af8194b595f9a2fb64ad034630786189335

--- include/hip/hiprtc.h
+++ include/hip/hiprtc.h
@@ -26,7 +26,7 @@ THE SOFTWARE.
 #if (defined(__HIP_PLATFORM_HCC__) || defined(__HIP_PLATFORM_AMD__)) && !(defined(__HIP_PLATFORM_NVCC__) || defined(__HIP_PLATFORM_NVIDIA__))
     #include <hip/amd_detail/hiprtc.h>
 #elif !(defined(__HIP_PLATFORM_HCC__) || defined(__HIP_PLATFORM_AMD__)) && (defined(__HIP_PLATFORM_NVCC__) || defined(__HIP_PLATFORM_NVIDIA__))
-    #include <hip/nvidia_detail/nvrtc.h>
+    #include <hip/nvidia_detail/hiprtc.h>
 #else
     #error("Must define exactly one of __HIP_PLATFORM_AMD__ or __HIP_PLATFORM_NVIDIA__");
 #endif
