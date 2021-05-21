6017ad8b8a76b9417bbf5ad2657cc7fbe6ab8ab2
Jatin Chaudhary <jatin.chaudhary@amd.com>
2021-05-21 02:31:28 -0700

    SWDEV-277697 - [CatchTest] Fix Documentation, Add test to AMD specific, Add HIP Macros, New Binary for multiproc tests
    Change-Id: I3783caf85c694b724ed55b778220b8ef9a39f84b
    
    
    [ROCm/hip commit: c088ddbe7c4f101093e60d028b570441afbf6a92]

--- tests/catch/hipTestMain/main.cc
+++ tests/catch/hipTestMain/main.cc
@@ -6,7 +6,7 @@ int main(int argc, char** argv) {
   auto& context = TestContext::get(argc, argv);
   if (context.skipTest()) {
     // CTest uses this regex to figure out if the test has been skipped
-    std::cout << "HIP_SKIP_THIS_TEST" << context.getCurrentTest() << std::endl;
+    std::cout << "HIP_SKIP_THIS_TEST" << std::endl;
     return 0;
   }
   return Catch::Session().run(argc, argv);
