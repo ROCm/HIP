f7de6b82f9c9d754faf34b8d8c202a7536843fde
Rupam Chetia <Rupam.Chetia@amd.com>
2021-05-28 21:02:27 +0530

    SWDEV-230423 - [dtest] Adding Stress tests.
    http://ontrack-internal.amd.com/browse/SWDEV-230423
    
    1. Moving stress folder from hip/test/src to hip/test.
    2. Adding Stream stress tests.
       These stress tests create multiple streams and launches kernel on them
       in multiple combinations for 100000 iterations. These tests will test
       the stability of streams created using hipStreamCreate, hipStreamCreateWithPriority
       and hipStreamCreateWithFlags.
    3. Adding printf stress tests using simple kernels.
    4. Adding printf stress tests using a complex kernel.
    
    Change-Id: Idcd26707fb9504ab8dbe6cebcbb32ade61bf1483
    
    
    [ROCm/hip commit: 1b53e0c51cdd1d2f1781eaf459d7e9691bc5c63b]

---
+++ tests/catch/stress/printf/printf_common.h
@@ -0,0 +1,99 @@
+/*
+Copyright (c) 2020 - present Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#ifndef _STRESSTEST_PRINTF_COMMON_H_
+#define _STRESSTEST_PRINTF_COMMON_H_
+
+#include <errno.h>
+#include <error.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <math.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+struct CaptureStream {
+  int saved_fd;
+  int orig_fd;
+  int temp_fd;
+
+  char tempname[13] = "mytestXXXXXX";
+
+  explicit CaptureStream(FILE *original) {
+    orig_fd = fileno(original);
+    saved_fd = dup(orig_fd);
+
+    if ((temp_fd = mkstemp(tempname)) == -1) {
+      error(0, errno, "Error");
+      assert(false);
+    }
+
+    fflush(nullptr);
+    if (dup2(temp_fd, orig_fd) == -1) {
+      error(0, errno, "Error");
+      assert(false);
+    }
+    if (close(temp_fd) != 0) {
+      error(0, errno, "Error");
+      assert(false);
+    }
+  }
+
+  void restoreStream() {
+    if (saved_fd == -1)
+      return;
+    fflush(nullptr);
+    if (dup2(saved_fd, orig_fd) == -1) {
+      error(0, errno, "Error");
+      assert(false);
+    }
+    if (close(saved_fd) != 0) {
+      error(0, errno, "Error");
+      assert(false);
+    }
+    saved_fd = -1;
+  }
+
+  const char *getTempFilename() {
+    return (const char*)tempname;
+  }
+
+  std::ifstream getCapturedData() {
+    restoreStream();
+    std::ifstream temp(tempname);
+    return temp;
+  }
+
+  ~CaptureStream() {
+    restoreStream();
+    if (remove(tempname) != 0) {
+      error(0, errno, "Error");
+      assert(false);
+    }
+  }
+};
+
+#endif  // _STRESSTEST_PRINTF_COMMON_H_
