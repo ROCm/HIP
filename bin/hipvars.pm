26dc58d72aa0aed1f9ca8af29cfd2f468c0df8e0
Maneesh Gupta <maneesh.gupta@amd.com>
2021-11-24 11:06:31 +0530

    Revert HIP version back to 4.4.0

--- bin/hipvars.pm
+++ bin/hipvars.pm
@@ -25,8 +25,8 @@ use Getopt::Long;
 use Cwd;
 use File::Basename;
 
-$HIP_BASE_VERSION_MAJOR = "5";
-$HIP_BASE_VERSION_MINOR = "0";
+$HIP_BASE_VERSION_MAJOR = "4";
+$HIP_BASE_VERSION_MINOR = "4";
 $HIP_BASE_VERSION_PATCH = "0";
 
 #---
