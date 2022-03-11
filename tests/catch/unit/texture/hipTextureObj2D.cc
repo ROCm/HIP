96acb821fe0f9d1c5c0287b262a2981a80201221
ROCm CI Service Account <66695075+rocm-ci@users.noreply.github.com>
2022-03-11 16:53:48 +0530

    SWDEV-321872 - [catch2][dtest] Texture tests migrated to catch2 (#2536)
    Change-Id: I4e29001812e2310680c6c24028667eb560194e87
    
    [ROCm/hip commit: 87eaccaf5919d39d43d7b072aff3688274cd214d]

---
+++ tests/catch/unit/texture/hipTextureObj2D.cc
@@ -0,0 +1,113 @@
+/*
+Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include <hip_test_common.hh>
+
+__global__ void tex2DKernel(float* outputData,
+                            hipTextureObject_t textureObject, int width) {
+#if !defined(__HIP_NO_IMAGE_SUPPORT) || !__HIP_NO_IMAGE_SUPPORT
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    outputData[y * width + x] = tex2D<float>(textureObject, x, y);
+#endif
+}
+
+TEST_CASE("Unit_hipTextureObj2D_Check") {
+#if HT_AMD
+  int imageSupport{};
+  HIP_CHECK(hipDeviceGetAttribute(&imageSupport,
+                           hipDeviceAttributeImageSupport, 0));
+  if (!imageSupport) {
+    INFO("Texture is not supported on the device. Test is skipped");
+    return;
+  }
+#endif
+  constexpr int SIZE = 256;
+  constexpr unsigned int width = SIZE;
+  constexpr unsigned int height = SIZE;
+  constexpr unsigned int size = width * height * sizeof(float);
+  unsigned int i, j;
+
+  float* dData = nullptr;
+  HIP_CHECK(hipMalloc(&dData, size));
+  REQUIRE(dData != nullptr);
+
+  float* hOutputData = reinterpret_cast<float*>(malloc(size));
+  REQUIRE(hOutputData != nullptr);
+  memset(hOutputData, 0, size);
+
+  float* hData = reinterpret_cast<float*>(malloc(size));
+  REQUIRE(hData != nullptr);
+  memset(hData, 0, size);
+  for (i = 0; i < height; i++) {
+    for (j = 0; j < width; j++) {
+      hData[i * width + j] = i * width + j;
+    }
+  }
+
+  hipChannelFormatDesc channelDesc =
+     hipCreateChannelDesc(32, 0, 0, 0, hipChannelFormatKindFloat);
+  hipArray* hipArray;
+  HIP_CHECK(hipMallocArray(&hipArray, &channelDesc, width, height));
+  HIP_CHECK(hipMemcpyToArray(hipArray, 0, 0, hData, size,
+                             hipMemcpyHostToDevice));
+
+  hipResourceDesc resDesc;
+  memset(&resDesc, 0, sizeof(resDesc));
+  resDesc.resType = hipResourceTypeArray;
+  resDesc.res.array.array = hipArray;
+
+  // Specify texture object parameters
+  hipTextureDesc texDesc;
+  memset(&texDesc, 0, sizeof(texDesc));
+  texDesc.addressMode[0] = hipAddressModeWrap;
+  texDesc.addressMode[1] = hipAddressModeWrap;
+  texDesc.filterMode = hipFilterModePoint;
+  texDesc.readMode = hipReadModeElementType;
+  texDesc.normalizedCoords = 0;
+
+  // Create texture object
+  hipTextureObject_t textureObject = 0;
+  HIP_CHECK(hipCreateTextureObject(&textureObject, &resDesc,
+                                   &texDesc, nullptr));
+
+  dim3 dimBlock(16, 16, 1);
+  dim3 dimGrid(width / dimBlock.x, height / dimBlock.y, 1);
+
+  hipLaunchKernelGGL(tex2DKernel, dim3(dimGrid), dim3(dimBlock),
+                     0, 0, dData, textureObject, width);
+
+  HIP_CHECK(hipDeviceSynchronize());
+  HIP_CHECK(hipMemcpy(hOutputData, dData, size, hipMemcpyDeviceToHost));
+
+  for (i = 0; i < height; i++) {
+    for (j = 0; j < width; j++) {
+      if (hData[i * width + j] != hOutputData[i * width + j]) {
+        INFO("Difference found at [ " << i << j << " ]: " <<
+              hData[i * width + j] << hOutputData[i * width + j]);
+        REQUIRE(false);
+      }
+    }
+  }
+
+  HIP_CHECK(hipDestroyTextureObject(textureObject));
+  HIP_CHECK(hipFree(dData));
+  HIP_CHECK(hipFreeArray(hipArray));
+  free(hData);
+}
