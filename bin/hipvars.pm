0a9f2c863b245cf75cc443245a849de5d6411c85
Maneesh Gupta <maneesh.gupta@amd.com>
2022-03-14 06:33:28 +0000

    SWDEV-1 - Bump patch version to 20590
    Change-Id: I52d3968c906f6386bb445b48d0330093321e0ce1

--- bin/hipvars.pm
+++ bin/hipvars.pm
@@ -27,7 +27,7 @@ use File::Basename;
 
 $HIP_BASE_VERSION_MAJOR = "5";
 $HIP_BASE_VERSION_MINOR = "1";
-$HIP_BASE_VERSION_PATCH = "20560";
+$HIP_BASE_VERSION_PATCH = "20590";
 
 #---
 # Function to parse config file
