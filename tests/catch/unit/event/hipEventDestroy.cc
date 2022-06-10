1dd5868c1bd6a4c58cbab1088375417ab91a58c5
Fábio Mestre <fabio.mestre@codeplay.com>
2022-06-10 16:12:50 +0100

    Fix review comments

--- tests/catch/unit/event/hipEventDestroy.cc
+++ tests/catch/unit/event/hipEventDestroy.cc
@@ -23,11 +23,12 @@ THE SOFTWARE.
 #include <hip_test_checkers.hh>
 #include <hip_test_kernels.hh>
 #include <hip_test_common.hh>
+#include "hip/hip_runtime_api.h"
 
 static constexpr size_t vectorSize{16384};
 
 
-static inline void launchVectorAdd(float*& A_h, float*& B_h, float*& C_h) {
+static inline void launchVectorAdd(float*& A_h, float*& B_h, float*& C_h, hipStream_t stream) {
   float* A_d{nullptr};
   float* B_d{nullptr};
   float* C_d{nullptr};
@@ -35,7 +36,7 @@ static inline void launchVectorAdd(float*& A_h, float*& B_h, float*& C_h) {
   HIP_CHECK(hipHostGetDevicePointer(reinterpret_cast<void**>(&A_d), A_h, 0));
   HIP_CHECK(hipHostGetDevicePointer(reinterpret_cast<void**>(&B_d), B_h, 0));
   HIP_CHECK(hipHostGetDevicePointer(reinterpret_cast<void**>(&C_d), C_h, 0));
-  HipTest::vectorADD<<<1, 1>>>(A_d, B_d, C_d, vectorSize);
+  HipTest::vectorADD<<<1, 1, 0, stream>>>(A_d, B_d, C_d, vectorSize);
 }
 
 
@@ -48,7 +49,7 @@ TEST_CASE("Unit_hipEventDestroy_Unfinished") {
   HIP_CHECK(hipEventCreate(&event));
 
   float *A_h, *B_h, *C_h;
-  launchVectorAdd(A_h, B_h, C_h);
+  launchVectorAdd(A_h, B_h, C_h, nullptr);
 
   HIP_CHECK(hipEventRecord(event));
   HIP_CHECK_ERROR(hipEventQuery(event), hipErrorNotReady);
@@ -64,11 +65,6 @@ TEST_CASE("Unit_hipEventDestroy_Unfinished") {
  *
  */
 TEST_CASE("Unit_hipEventDestroy_WithWaitingStream") {
-  float *A_d, *B_d, *C_d;
-  HIP_CHECK(hipMalloc(reinterpret_cast<void**>(&A_d), vectorSize));
-  HIP_CHECK(hipMalloc(reinterpret_cast<void**>(&B_d), vectorSize));
-  HIP_CHECK(hipMalloc(reinterpret_cast<void**>(&C_d), vectorSize));
-
   hipEvent_t event;
   HIP_CHECK(hipEventCreate(&event));
 
@@ -76,7 +72,7 @@ TEST_CASE("Unit_hipEventDestroy_WithWaitingStream") {
   HIP_CHECK(hipStreamCreate(&stream));
 
   float *A_h, *B_h, *C_h;
-  launchVectorAdd(A_h, B_h, C_h);
+  launchVectorAdd(A_h, B_h, C_h, stream);
 
   HIP_CHECK(hipEventRecord(event, stream));
   HIP_CHECK_ERROR(hipEventQuery(event), hipErrorNotReady);
@@ -84,24 +80,27 @@ TEST_CASE("Unit_hipEventDestroy_WithWaitingStream") {
   HIP_CHECK(hipEventDestroy(event));
   HIP_CHECK_ERROR(hipStreamQuery(stream), hipErrorNotReady);
   HIP_CHECK(hipStreamSynchronize(stream));
+  HIP_CHECK(hipStreamDestroy(stream));
 
   HipTest::checkVectorADD(A_h, B_h, C_h, vectorSize);
   HipTest::freeArraysForHost(A_h, B_h, C_h, true);
 }
 
 TEST_CASE("Unit_hipEventDestroy_Negative") {
-  
+#if HT_AMD
+  HipTest::HIP_SKIP_TEST("EXSWCPHIPT-105");
+  return;
+#endif
+
   SECTION("Invalid Event") {
     hipEvent_t event{nullptr};
     HIP_CHECK_ERROR(hipEventDestroy(event), hipErrorInvalidResourceHandle);
   }
 
-#ifndef HT_AMD
   SECTION("Destroy twice") {
     hipEvent_t event;
     HIP_CHECK(hipEventCreate(&event));
     HIP_CHECK(hipEventDestroy(event));
     HIP_CHECK_ERROR(hipEventDestroy(event), hipErrorContextIsDestroyed);
   }
-#endif
 }
