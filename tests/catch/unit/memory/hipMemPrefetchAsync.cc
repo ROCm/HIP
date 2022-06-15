480b3abbf320144bdcb91efc50ff18c9c0724ce8
ROCm CI Service Account <66695075+rocm-ci@users.noreply.github.com>
2022-06-15 23:28:10 +0530

    SWDEV-339657 - Update catch codes with correct coordinates (#2735)
    Change-Id: I8ca78d770a742bf6c2abede494759caa923fcc19
    
    
    [ROCm/hip commit: 0cf0ead620899b649fb4cef8a959527b588e9b6d]

--- tests/catch/unit/memory/hipMemPrefetchAsync.cc
+++ tests/catch/unit/memory/hipMemPrefetchAsync.cc
@@ -20,8 +20,8 @@ THE SOFTWARE.
 #include <hip_test_common.hh>
 // Kernel function
 __global__ void MemPrftchAsyncKernel(int* C_d, const int* A_d, size_t N) {
-  size_t offset = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x);
-  size_t stride = hipBlockDim_x * hipGridDim_x;
+  size_t offset = (blockIdx.x * blockDim.x + threadIdx.x);
+  size_t stride = blockDim.x * gridDim.x;
   for (size_t i = offset; i < N; i += stride) {
     C_d[i] = A_d[i] * A_d[i];
   }
