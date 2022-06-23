ef614d5e783fc4b359dec37fb578a37f8b86454d
Jatin Chaudhary <jatin.chaudhary@codeplay.com>
2022-06-23 15:19:17 +0100

    fix bugs

--- tests/catch/unit/printf/printfSpecifiers.cc
+++ tests/catch/unit/printf/printfSpecifiers.cc
@@ -89,7 +89,7 @@ x
 )here");
 #endif
 
-  hip::SpawnProc proc("printfSepcifiers", true);
+  hip::SpawnProc proc("printfSpecifiers", true);
   REQUIRE(0 == proc.run());
   REQUIRE(proc.getOutput() == reference);
 }
