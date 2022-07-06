ea29d80b9eac156317aa574aa02b2edbde20b079
ShanoToni <anton_b_mitkov@abv.bg>
2022-07-06 16:50:50 +0100

    Sync memcpy testing passing on Nvidia devices

--- tests/catch/unit/memory/hipMemcpySync.cc
+++ tests/catch/unit/memory/hipMemcpySync.cc
@@ -39,13 +39,13 @@ static void checkForHostSync(hipStream_t stream, bool async, allocType type) {
   } else {
     REQUIRE(true);
   }
-  // if (async && type == allocType::deviceMalloc) {
-  //   HIP_CHECK_ERROR(hipStreamQuery(stream), hipErrorNotReady);
-  // } else if (!stream && type == allocType::deviceMalloc && !async) {
-  //   HIP_CHECK_ERROR(hipStreamQuery(stream), hipErrorNotReady);
-  // } else {
-  //   HIP_CHECK(hipStreamQuery(stream));
-  // }
+  //if (async && type == allocType::deviceMalloc) {
+  //  HIP_CHECK_ERROR(hipStreamQuery(stream), hipErrorNotReady);
+  //} else if (!stream && type == allocType::deviceMalloc && !async) {
+  //  HIP_CHECK_ERROR(hipStreamQuery(stream), hipErrorNotReady);
+  //} else {
+  //  HIP_CHECK(hipStreamQuery(stream));
+  //}
 }
 
 static void checkForDeviceSync(hipStream_t stream) { HIP_CHECK(hipStreamQuery(stream)); }
@@ -54,12 +54,7 @@ static void checkForDeviceSync(hipStream_t stream) { HIP_CHECK(hipStreamQuery(st
 template <typename T>
 static void runMemcpyTests(hipStream_t stream, bool async, allocType type, memType memType,
                            MultiDData data) {
-  using std::chrono::duration;
-  using std::chrono::duration_cast;
-  using std::chrono::high_resolution_clock;
-  using std::chrono::milliseconds;
-  // bool fromHost = GENERATE(true, false);
-  bool fromHost = true;
+  bool fromHost = GENERATE(true, false);
 
   size_t dataH = data.height == 0 ? 1 : data.height;
   size_t dataD = data.depth == 0 ? 1 : data.depth;
@@ -69,17 +64,9 @@ static void runMemcpyTests(hipStream_t stream, bool async, allocType type, memTy
   createFillerData<T>(sizeInBytes, testValue);
   CAPTURE(type, memType, data.width, data.height, data.depth, stream, async, fromHost, sizeInBytes);
 
-  auto t1 = high_resolution_clock::now();
-  launchLongRunningKernel(1000, stream);
+  launchLongRunningKernel(100, stream);
   if (fromHost) {
     hostMemcpyCheck(aPtr.first, type, testValue, memType, data, stream, async);
-    auto t2 = high_resolution_clock::now();
-    printf("Time for a longRunningKernel with : \t%lu ms \n", duration_cast<milliseconds>(t2 - t1));
-    printf(
-        " Type: \t\t %lu \n Async: \t %lu \n Data W,H,D: \t[ %lu, %lu, %lu ]\n "
-        "Stream: \t %lu \n",
-        type, async, data.width, data.height, data.depth, stream);
-
     checkForHostSync(stream, async, type);
 
   } else {
@@ -101,38 +88,32 @@ TEST_CASE("Unit_hipMemcpySync") {
                             allocType::devRegistered);
   memType memcpy_type = memType::hipMem;
   MultiDData data;
-  // data.width = GENERATE(1, 1024);
-  data.width = GENERATE(1024);
+  data.width = GENERATE(1, 32);
+
   doMemTest<char>(runMemcpyTests<char>, type, memcpy_type, data);
 }
 
 TEST_CASE("Unit_hipMemcpy2DSync") {
-  //allocType mallocType = GENERATE(allocType::deviceMalloc, allocType::hostMalloc,
-  //                                allocType::hostRegisted, allocType::devRegistered);
-  allocType mallocType = allocType::deviceMalloc;
-  // allocType mallocType = allocType::deviceMalloc;
+  allocType mallocType = GENERATE(allocType::deviceMalloc, allocType::hostMalloc,
+                                  allocType::hostRegisted, allocType::devRegistered);
+
   memType memcpy_type = memType::hipMem2D;
   MultiDData data;
-  data.width = GENERATE(1, 1024);
-  data.height = GENERATE(1, 1024);
-  
+  data.width = GENERATE(1, 32);
+  data.height = GENERATE(1, 32);
 
   doMemTest<char>(runMemcpyTests<char>, mallocType, memcpy_type, data);
 }
 
 TEST_CASE("Unit_hipMemcpy3DSync") {
-  // allocType mallocType = GENERATE(allocType::deviceMalloc, allocType::hostMalloc,
-  //                                 allocType::hostRegisted, allocType::devRegistered);
-  allocType mallocType = allocType::deviceMalloc;
+  allocType mallocType = GENERATE(allocType::deviceMalloc, allocType::hostMalloc,
+                                  allocType::hostRegisted, allocType::devRegistered);
+
   memType memcpy_type = memType::hipMem3D;
   MultiDData data;
-  //data.width = GENERATE(1, 256);
-  //data.height = GENERATE(1, 256);
-  //data.depth = GENERATE(1, 256);
-   data.width = 128;
-   data.height = 128;
-   data.depth = 129;
-
+  data.width = GENERATE(1, 8);
+  data.height = GENERATE(1, 8);
+  data.depth = GENERATE(1, 8);
 
   doMemTest<char>(runMemcpyTests<char>, mallocType, memcpy_type, data);
 }
