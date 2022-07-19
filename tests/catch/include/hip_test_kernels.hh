780f48f14eb268fe095607c7b8da7363c99d2127
ROCm CI Service Account <66695075+rocm-ci@users.noreply.github.com>
2022-07-19 19:27:29 +0530

    SWDEV-306122 - [catch2][dtest] API hipGraphKernelNodeGetParams, hipGraphKernelNodeSetParams, hipGraphExecKernelNodeSetParams. (#2802)
    Change-Id: Ie5eb8949f589ac7ac5eb0f02e99f2012d9c0b926
    
    [ROCm/hip commit: 978b02c52f39f05393f6534334e530ac034eac97]

--- tests/catch/include/hip_test_kernels.hh
+++ tests/catch/include/hip_test_kernels.hh
@@ -34,6 +34,14 @@ template <typename T> __global__ void vectorADD(const T* A_d, const T* B_d, T* C
   }
 }
 
+template <typename T> __global__ void vectorSUB(const T* A_d, const T* B_d, T* C_d, size_t NELEM) {
+  size_t offset = (blockIdx.x * blockDim.x + threadIdx.x);
+  size_t stride = blockDim.x * gridDim.x;
+
+  for (size_t i = offset; i < NELEM; i += stride) {
+    C_d[i] = A_d[i] - B_d[i];
+  }
+}
 
 template <typename T>
 __global__ void vectorADDReverse(const T* A_d, const T* B_d, T* C_d, size_t NELEM) {
