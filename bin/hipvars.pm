3784b8216eb46e8561929263380f02ebb53c22fa
Maneesh Gupta <maneesh.gupta@amd.com>
2022-08-18 09:36:19 +0000

    SWDEV-352359 - Bump HIP_VERSION_PATCH to 22061
    Change-Id: Ie32d3fa728d03921ab3a51dcde7edf3a3fe6fc50

--- bin/hipvars.pm
+++ bin/hipvars.pm
@@ -27,7 +27,7 @@ use File::Basename;
 
 $HIP_BASE_VERSION_MAJOR = "5";
 $HIP_BASE_VERSION_MINOR = "3";
-$HIP_BASE_VERSION_PATCH = "22060";
+$HIP_BASE_VERSION_PATCH = "22061";
 
 #---
 # Function to parse config file
