739d21f215fcf5cdc4e5207c0015a8d67626059a
Dino Music <dino.music@htecgroup.com>
2022-08-24 09:05:35 +0200

    Added include guard.
    Signed-off-by: Dino Music <dino.music@htecgroup.com>
    Co-authored-by: Mirza Halilcevic <mirza.halilcevic@htecgroup.com>

--- tests/catch/unit/errorHandling/errorEnumerators.h
+++ tests/catch/unit/errorHandling/errorEnumerators.h
@@ -20,6 +20,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+#pragma once
+
 #include <hip/hip_runtime_api.h>
 
 constexpr hipError_t kErrorEnumerators[] = {hipSuccess,
