1230a9f56acebd343eb8b419584e169a2287b36b
Dino Music <dino.music@htecgroup.com>
2022-08-24 12:30:15 +0200

    Remove threaded_zig_zag_test.
    Signed-off-by: Dino Music <dino.music@htecgroup.com>

--- tests/catch/unit/device/hipSetGetDevice.cc
+++ tests/catch/unit/device/hipSetGetDevice.cc
@@ -6,8 +6,10 @@ in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -23,6 +25,7 @@ THE SOFTWARE.
  */
 
 #include <thread>
+
 #include <hip_test_common.hh>
 #include <threaded_zig_zag_test.hh>
 
