b7798d0f9107350687d463e099264dea9e242b17
Mirza Halilcevic <mirza.halilcevic@htecgroup.com>
2022-09-05 04:16:49 -0400

    EXSWHTEC-26 - Reorder tests and generate test cases for all available devices in certain tests.

--- tests/catch/unit/device/hipDeviceSetGetCacheConfig.cc
+++ tests/catch/unit/device/hipDeviceSetGetCacheConfig.cc
@@ -32,18 +32,30 @@ constexpr std::array<hipFuncCache_t, 4> kCacheConfigs{
 
 
 TEST_CASE("Unit_hipDeviceSetCacheConfig_Positive_Basic") {
+  const auto device = GENERATE(range(0, HipTest::getDeviceCount()));
+  HIP_CHECK(hipSetDevice(device));
+  INFO("Current device is: " << device);
+
   const auto cache_config =
       GENERATE(from_range(std::begin(kCacheConfigs), std::end(kCacheConfigs)));
   HIP_CHECK(hipDeviceSetCacheConfig(cache_config));
 }
 
 TEST_CASE("Unit_hipDeviceGetCacheConfig_Positive_Default") {
+  const auto device = GENERATE(range(0, HipTest::getDeviceCount()));
+  HIP_CHECK(hipSetDevice(device));
+  INFO("Current device is: " << device);
+
   hipFuncCache_t cache_config;
   HIP_CHECK(hipDeviceGetCacheConfig(&cache_config));
   REQUIRE(cache_config == hipFuncCachePreferNone);
 }
 
 TEST_CASE("Unit_hipDeviceGetCacheConfig_Positive_Basic") {
+  const auto device = GENERATE(range(0, HipTest::getDeviceCount()));
+  HIP_CHECK(hipSetDevice(device));
+  INFO("Current device is: " << device);
+  
   const auto cache_config =
       GENERATE(from_range(std::begin(kCacheConfigs), std::end(kCacheConfigs)));
 
