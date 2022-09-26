1c9f3fc9f03a85b8c2279539d6961501e64e5ec5
nives-vukovic <110852104+nives-vukovic@users.noreply.github.com>
2022-09-26 07:33:30 +0200

    EXSWHTEC-37 - Expand tests for hipStreamQuery (#2921)
    - Add testing for null stream in Unit_hipStreamQuery_WithNoWork
    - Add testing for null stream in Unit_hipStreamQuery_WithFinishedWork
    
    [ROCm/hip commit: 4afc7866b5f8491e9b5bb6724990715e0380ec97]

--- tests/catch/unit/stream/hipStreamQuery.cc
+++ tests/catch/unit/stream/hipStreamQuery.cc
@@ -26,9 +26,16 @@ THE SOFTWARE.
  **/
 TEST_CASE("Unit_hipStreamQuery_WithNoWork") {
   hipStream_t stream{nullptr};
-  HIP_CHECK(hipStreamCreate(&stream));
-  HIP_CHECK(hipStreamQuery(stream));
-  HIP_CHECK(hipStreamDestroy(stream));
+
+  SECTION("Null Stream") {
+    HIP_CHECK(hipStreamQuery(stream));
+  }
+
+  SECTION ("Created Stream") {
+    HIP_CHECK(hipStreamCreate(&stream));
+    HIP_CHECK(hipStreamQuery(stream));
+    HIP_CHECK(hipStreamDestroy(stream));
+  }
 }
 
 /**
@@ -37,13 +44,22 @@ TEST_CASE("Unit_hipStreamQuery_WithNoWork") {
  **/
 TEST_CASE("Unit_hipStreamQuery_WithFinishedWork") {
   hipStream_t stream{nullptr};
-  HIP_CHECK(hipStreamCreate(&stream));
 
-  hip::stream::empty_kernel<<<dim3(1), dim3(1), 0, stream>>>();
-  HIP_CHECK(hipStreamSynchronize(stream));
+  SECTION("Null Stream") {
+    hip::stream::empty_kernel<<<dim3(1), dim3(1), 0, stream>>>();
+    HIP_CHECK(hipStreamSynchronize(stream));
 
-  HIP_CHECK(hipStreamQuery(stream));
-  HIP_CHECK(hipStreamDestroy(stream));
+    HIP_CHECK(hipStreamQuery(stream));
+  }
+
+  SECTION ("Created Stream") {
+    HIP_CHECK(hipStreamCreate(&stream));
+    hip::stream::empty_kernel<<<dim3(1), dim3(1), 0, stream>>>();
+    HIP_CHECK(hipStreamSynchronize(stream));
+
+    HIP_CHECK(hipStreamQuery(stream));
+    HIP_CHECK(hipStreamDestroy(stream));
+  }
 }
 
 #if !HT_NVIDIA
