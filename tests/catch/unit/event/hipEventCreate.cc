4b74ce9c651b9f0a906bf9387d4b8153ed846867
nives-vukovic <110852104+nives-vukovic@users.noreply.github.com>
2022-11-02 17:02:58 +0100

    EXSWHTEC-13 - Implement positive test for hipEventCreate (#2901)
    - Add hipEventCreate.cc file with a simple positive unit test
    - Add newly created file into CMakeLists.txt
    - Update hipEventCreate test according to coding guidelines
    
    [ROCm/hip commit: 5664a6299f75e770c876017f93b08f96d8e98ae8]

---
+++ tests/catch/unit/event/hipEventCreate.cc
@@ -0,0 +1,38 @@
+/*
+Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+/*
+Testcase Scenarios :
+Unit_hipEventCreate_Positive - Test simple event creation with hipEventCreate api
+*/
+
+#include <hip_test_common.hh>
+
+TEST_CASE("Unit_hipEventCreate_Positive") {
+  int id = GENERATE(range(0, HipTest::getDeviceCount()));
+  HIP_CHECK(hipSetDevice(id));
+
+  hipEvent_t event;
+  HIP_CHECK(hipEventCreate(&event));
+  REQUIRE(event != nullptr);
+
+  HIP_CHECK(hipEventDestroy(event));
+}
