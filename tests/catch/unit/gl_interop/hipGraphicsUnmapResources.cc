02f2f77a2ff2369e3af879e44305696e98132fdd
Mirza Halilcevic <mirza.halilcevic@htecgroup.com>
2022-11-15 17:11:42 +0000

    EXSWHTEC-140 - Implement tests for hipGraphicsUnmapResources - Negative test case for different streams - Negative parameter tests.

---
+++ tests/catch/unit/gl_interop/hipGraphicsUnmapResources.cc
@@ -0,0 +1,96 @@
+/*
+Copyright (c) 2022 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include <hip_test_common.hh>
+#include <hip/hip_runtime_api.h>
+
+#include "gl_interop_common.hh"
+
+#include "GLContextScopeGuard.hh"
+
+TEST_CASE("Unit_hipGraphicsUnmapResources_Negative_Different_Streams") {
+  GLContextScopeGuard gl_context;
+
+  CreateGLBufferObject();
+
+  hipGraphicsResource* vbo_resource;
+
+  HIP_CHECK(hipGraphicsGLRegisterBuffer(&vbo_resource, vbo, hipGraphicsRegisterFlagsNone));
+
+  HIP_CHECK(hipGraphicsMapResources(1, &vbo_resource, 0));
+
+  hipStream_t stream;
+  HIP_CHECK(hipStreamCreate(&stream));
+  HIP_CHECK_ERROR(hipGraphicsUnmapResources(1, &vbo_resource, stream), hipErrorNotMapped);
+  HIP_CHECK(hipStreamDestroy(stream));
+
+  HIP_CHECK(hipGraphicsUnmapResources(1, &vbo_resource, 0));
+
+  HIP_CHECK(hipGraphicsUnregisterResource(vbo_resource));
+}
+
+TEST_CASE("Unit_hipGraphicsUnmapResources_Negative_Parameters") {
+  GLContextScopeGuard gl_context;
+
+  CreateGLBufferObject();
+
+  hipGraphicsResource* vbo_resource;
+
+  HIP_CHECK(hipGraphicsGLRegisterBuffer(&vbo_resource, vbo, hipGraphicsRegisterFlagsNone));
+
+  HIP_CHECK(hipGraphicsMapResources(1, &vbo_resource, 0));
+
+  SECTION("count == 0") {
+    HIP_CHECK_ERROR(hipGraphicsUnmapResources(0, &vbo_resource, 0), hipErrorInvalidHandle);
+  }
+
+  SECTION("count < 0") {
+    HIP_CHECK_ERROR(hipGraphicsUnmapResources(-1, &vbo_resource, 0), hipErrorInvalidHandle);
+  }
+
+  SECTION("resources == nullptr") {
+    HIP_CHECK_ERROR(hipGraphicsUnmapResources(1, nullptr, 0), hipErrorInvalidHandle);
+  }
+
+  SECTION("non-registered resource") {
+    hipGraphicsResource* unregistered_resource;
+    HIP_CHECK_ERROR(hipGraphicsUnmapResources(1, &unregistered_resource, 0), hipErrorInvalidHandle);
+  }
+
+  SECTION("non-mapped resource") {
+    hipGraphicsResource* unmapped_resource;
+    HIP_CHECK(hipGraphicsGLRegisterBuffer(&unmapped_resource, vbo, hipGraphicsRegisterFlagsNone));
+    HIP_CHECK_ERROR(hipGraphicsUnmapResources(1, &unmapped_resource, 0), hipErrorNotMapped);
+    HIP_CHECK(hipGraphicsUnregisterResource(unmapped_resource));
+  }
+
+  SECTION("different stream") {
+    hipStream_t stream;
+    HIP_CHECK(hipStreamCreate(&stream));
+    HIP_CHECK_ERROR(hipGraphicsUnmapResources(1, &vbo_resource, stream), hipErrorNotMapped);
+    HIP_CHECK(hipStreamDestroy(stream));
+  }
+
+  HIP_CHECK(hipGraphicsUnmapResources(1, &vbo_resource, 0));
+
+  HIP_CHECK(hipGraphicsUnregisterResource(vbo_resource));
+}
