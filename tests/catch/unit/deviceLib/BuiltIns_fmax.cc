5c85bc7ce78e8673dd546e2d776f577eb68bcdd9
ROCm CI Service Account <66695075+rocm-ci@users.noreply.github.com>
2022-11-17 03:31:05 +0530

    SWDEV-346579 - atomics test cases for non MI2xx/MI3xx (#3060)
    Replace the internal __builtin__ functions with external APIs
    
    Change-Id: Ia3cf37af004b5b9a4833b18a8771a8864772beb4
    
    
    [ROCm/hip commit: 0acac9c7db45c8e3c107198eebfa83cb031715c4]

--- tests/catch/unit/deviceLib/BuiltIns_fmax.cc
+++ tests/catch/unit/deviceLib/BuiltIns_fmax.cc
@@ -39,20 +39,12 @@ __global__ void unsafeAtomicMax_FlatMem(double* addr, double* result) {
   __shared__ double int_val;
   int_val = 5;
   double comp = 10;
-  if (__builtin_amdgcn_is_shared(
-        (const __attribute__((address_space(0))) void*)(&int_val)))
-    *result = __builtin_amdgcn_flat_atomic_fmax_f64(&int_val, comp);
-  else
-    *result = __builtin_amdgcn_global_atomic_fmax_f64(&int_val, comp);
+  *result = unsafeAtomicMax(&int_val, comp);
   *addr = int_val;
 }
 __global__ void unsafeAtomicMax_GlobalMem(double* addr, double* result) {
   double comp = 10;
-  if (__builtin_amdgcn_is_shared(
-        (const __attribute__((address_space(0))) void*)(addr)))
-    *result =  __builtin_amdgcn_flat_atomic_fmax_f64(addr, comp);
-  else
-    *result =  __builtin_amdgcn_global_atomic_fmax_f64(addr, comp);
+  *result = unsafeAtomicMax(addr, comp);
 }
 static constexpr auto fmaxFlatMem {
 R"(
@@ -61,11 +53,7 @@ __global__ void unsafeAtomicMax_FlatMem(double* addr, double* result) {
   __shared__ double int_val;
   int_val = 5;
   double comp = 10;
-  if (__builtin_amdgcn_is_shared(
-        (const __attribute__((address_space(0))) void*)(&int_val)))
-    *result = __builtin_amdgcn_flat_atomic_fmax_f64(&int_val, comp);
-  else
-    *result = __builtin_amdgcn_global_atomic_fmax_f64(&int_val, comp);
+  *result = unsafeAtomicMax(&int_val, comp);
   *addr = int_val;
 }
 )"};
@@ -75,11 +63,7 @@ R"(
 extern "C"
 __global__ void unsafeAtomicMax_GlobalMem(double* addr, double* result) {
   double comp = 10;
-  if (__builtin_amdgcn_is_shared(
-        (const __attribute__((address_space(0))) void*)(addr)))
-    *result =  __builtin_amdgcn_flat_atomic_fmax_f64(addr, comp);
-  else
-    *result =  __builtin_amdgcn_global_atomic_fmax_f64(addr, comp);
+  *result = unsafeAtomicMax(addr, comp);
 }
 )"};
 
