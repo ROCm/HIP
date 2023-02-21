62fb0de79031a13b0dda55004ec66434c9b4d30d
Saad Rahim <saad.rahim@amd.com>
2023-02-21 16:57:35 -0700

    Restructuring

---
+++ docs/doxygen/README.md
@@ -0,0 +1,4 @@
+# Doxygen Instructions
+
+Please run the following command to build the doxygen based documentation.
+```doxygen Doxyfile```
---
+++ docs/sphinx/conf.py
@@ -0,0 +1,14 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from rocm_docs import ROCmDocs
+
+docs_core = ROCmDocs("ROCm Documentation")
+docs_core.run_doxygen()
+docs_core.setup()
+
+for sphinx_var in ROCmDocs.SPHINX_VARS:
+    globals()[sphinx_var] = getattr(docs_core, sphinx_var)
