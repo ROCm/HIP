f3d8fc881cacdea8c6dd33ec7733224eb2513a58
Maneesh Gupta <maneesh.gupta@amd.com>
2023-04-27 13:58:40 +0000

    SWDEV-397344 - Bump patch version to 30202
    Change-Id: Ida2d603006218a8c74fd58b81830ac7802ab58d1

--- bin/hipvars.pm
+++ bin/hipvars.pm
@@ -27,7 +27,7 @@ use File::Basename;
 
 $HIP_BASE_VERSION_MAJOR = "5";
 $HIP_BASE_VERSION_MINOR = "5";
-$HIP_BASE_VERSION_PATCH = "30201";
+$HIP_BASE_VERSION_PATCH = "30202";
 
 #---
 # Function to parse config file
