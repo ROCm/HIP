50d75440c0faff8c42b51f86f15a9309c3e5ffb7
arjun-raj-kuppala <60718144+arjun-raj-kuppala@users.noreply.github.com>
2023-08-31 20:16:52 +0530

    Create nvidia.sh

---
+++ .jenkins/nvidia.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+export WORKSPACE=$PWD/hipanl
+rm -rf $WORKSPACE
+mkdir $WORKSPACE
+cd ${WORKSPACE}
+               
+            
+cd ${WORKSPACE}/clr
+rm -rf build
+mkdir -p build
+cd build
+
+cmake -DCLR_BUILD_HIP=ON -DHIP_PLATFORM=nvidia -DHIPCC_BIN_DIR=$HIPCC_DIR/bin -DHIP_COMMON_DIR=$HIP_DIR -DCMAKE_INSTALL_PREFIX=$PWD/install ..
+
+
+make -j$(nproc)
+make install  -j$(nproc)
+
+
+cd ${WORKSPACE}/hip-tests
+export HIP_PATH="${CLR_DIR}"/build/install
+
+rm -rf build
+mkdir -p build
+cd build
+echo "testing $HIP_PATH"
+
+
+export HIP_PLATFORM=nvidia
+cmake -DHIP_PLATFORM=nvidia -DHIP_PATH=$CLR_DIR/build/install ../catch
+
+
+make -j$(nproc) build_tests
+
+ 
+
+cd ${WORKSPACE}/hip-tests
+cd build
+ctest --overwrite BuildDirectory=. --output-junit hiptest_output_catch_nvidia.xml -E 'Unit_hipMemcpyHtoD_Positive_Synchronization_Behavior|Unit_hipMemcpy_Positive_Synchronization_Behavior|Unit_hipFreeNegativeHost'
+
+
+
