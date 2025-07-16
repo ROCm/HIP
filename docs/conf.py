0a890c5878f82ce50b9c155abcfc4b7b81b8e1a6
Brian Kocoloski <brian.kocoloski@amd.com>
2025-07-16 12:56:41 -0400

    address feedback

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,8 @@ exclude_patterns = [
     "doxygen/mainpage.md",
     "understand/glossary.md",
     'how-to/debugging_env.rst',
-    "data/env_variables_hip.rst"
+    "data/env_variables_hip.rst",
+    "venv"
 ]
 
 git_url = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']).strip().decode('ascii')
