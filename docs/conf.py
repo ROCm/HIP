2fe0eca1f99372e9ebe28f4003f97005690f26a2
randyh62 <42045079+randyh62@users.noreply.github.com>
2025-07-24 11:39:46 -0700

    Update conf.py
    Update exclude_patterns in conf.py for relocated env_variables include files

--- docs/conf.py
+++ docs/conf.py
@@ -54,10 +54,10 @@ suppress_warnings = ["etoc.toctree"]
 numfig = False
 
 exclude_patterns = [
-    "doxygen/mainpage.md",
-    "understand/glossary.md",
-    'how-to/debugging_env.rst',
-    "data/reference/env_variables"
+    "./doxygen/mainpage.md",
+    "./understand/glossary.md",
+    './how-to/debugging_env.rst',
+    "./reference/env_variables"
 ]
 
 git_url = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']).strip().decode('ascii')
