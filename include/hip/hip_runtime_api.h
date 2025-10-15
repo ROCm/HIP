691f2985177584e7ff97ef207fb7752e875d3197
JeniferC99 <150404595+JeniferC99@users.noreply.github.com>
2025-10-15 13:32:41 -0700

    Revert "SWDEV-554608 - Add hipHostRegisterIoMemory for hipHostRegister (#1266…" (#1397)
    This reverts commit 7f746fc540c334b5c7098b994df709e0c99cc1e3.

--- include/hip/hip_runtime_api.h
+++ include/hip/hip_runtime_api.h
@@ -899,10 +899,7 @@ enum hipLimit_t {
  * can be obtained with #hipHostGetDevicePointer.*/
 #define hipHostRegisterMapped 0x2
 
-/** The passed memory pointer is treated as pointing to some memory-mapped I/O space, e.g.
- * belonging to a third-party PCIe device, and it will be marked as non cache-coherent and 
- * contiguous.
- * */
+/** Not supported.*/
 #define hipHostRegisterIoMemory 0x4
 
 /** This flag is ignored On AMD devices.*/
