edfddc2be105c04930da8d76d101c44e752121c1
Julia Jiang <56359287+jujiang-del@users.noreply.github.com>
2026-05-04 21:00:49 +0000

    [HIP][DOC]: Adding more descriptions for device attribute  number-of-xccs (#5622)
    ## Motivation
    
    Adding descriptions for the enum hipDeviceAttributeNumberOfXccs
    
    ## Technical Details
    
    Adding details for hipDeviceAttributeNumberOfXccs
    
    ## JIRA ID
    
    https://amd-hub.atlassian.net/browse/AIRUNTIME-2106
    
    ## Test Plan
    
    N/A
    
    ## Test Result
    
    N/A
    
    ## Submission Checklist
    
    - Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    [rocm-systems] ROCm/rocm-systems#5622 (commit 5d2f6fc)

--- include/hip/hip_runtime_api.h
+++ include/hip/hip_runtime_api.h
@@ -581,7 +581,13 @@ typedef enum hipDeviceAttribute_t {
                                                   ///< units for the device
   hipDeviceAttributeFineGrainSupport,  ///< '1' if Device supports fine grain, '0' otherwise
   hipDeviceAttributeWallClockRate,     ///< Constant frequency of wall clock in kilohertz.
-  hipDeviceAttributeNumberOfXccs,      ///< The number of XCC(s) on the device
+  hipDeviceAttributeNumberOfXccs,  ///< Number of XCC(s) in the device’s current
+                                   ///< compute partition(XCP).
+                                   ///< An XCC is AMD’s fundamental GPU compute tile.
+                                   ///< Devices like MI300X include multiple XCCs
+                                   ///< per package. Value varies by boot‑time
+                                   ///< partition mode(compute or memory).
+                                   ///< Partition modes can be set using amd‑smi.
   hipDeviceAttributeMaxAvailableVgprsPerThread,  ///< Max number of available (directly or
                                                  ///< indirectly addressable) VGPRs per thread in
                                                  ///< DWORDs.
