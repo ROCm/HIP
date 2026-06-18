3b210b6a0fa6b9a1ce0bb0dbe8102127a4efc53c
Julia Jiang <56359287+jujiang-del@users.noreply.github.com>
2026-06-18 15:31:24 +0000

    [HIP][DOC] - Adjust Doxygen parameter annoatations in HIP  runtime APIs (#6127)
    ## Motivation
    
    Adjust Doxygen parameter annotations in HIP runtime APIs
    
    ## Technical Details
    
    Updated the HIP API header file for Doxygen parameter annotations as
    requested.
    
    ## JIRA ID
    
    https://amd-hub.atlassian.net/browse/AIRUNTIME-2168
    
    ## Test Plan
    
    Build HIP documentation and verify API parameter annotation
    
    ## Test Result
    
    Should be adjusted as requested.
    
    ## Submission Checklist
    
    - Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    [rocm-systems] ROCm/rocm-systems#6127 (commit 43947f7)

--- include/hip/hip_runtime_api.h
+++ include/hip/hip_runtime_api.h
@@ -2830,7 +2830,7 @@ hipError_t hipDrvGetErrorString(hipError_t hipError, const char** errorString);
 /**
  * @brief Creates an asynchronous stream.
  *
- * @param[in, out] stream  Valid pointer to hipStream_t.  This function writes the memory with the
+ * @param[out] stream  Valid pointer to hipStream_t.  This function writes the memory with the
  * newly created stream.
  * @returns #hipSuccess, #hipErrorInvalidValue
  *
@@ -2849,7 +2849,7 @@ hipError_t hipStreamCreate(hipStream_t* stream);
 /**
  * @brief Creates an asynchronous stream with flag.
  *
- * @param[in, out] stream  Pointer to new stream
+ * @param[out] stream  Pointer to new stream
  * @param[in] flags  Parameters to control stream creation
  * @returns #hipSuccess, #hipErrorInvalidValue
  *
@@ -2870,7 +2870,7 @@ hipError_t hipStreamCreateWithFlags(hipStream_t* stream, unsigned int flags);
 /**
  * @brief Creates an asynchronous stream with the specified priority.
  *
- * @param[in, out] stream  Pointer to new stream
+ * @param[out] stream  Pointer to new stream
  * @param[in] flags  Parameters to control stream creation
  * @param[in] priority  Priority of the stream. Lower numbers represent higher priorities.
  * @returns #hipSuccess, #hipErrorInvalidValue
@@ -3037,7 +3037,7 @@ hipError_t hipStreamGetDevice(hipStream_t stream, hipDevice_t* device);
 /**
  * @brief Creates an asynchronous stream with the specified CU mask.
  *
- * @param[in, out] stream  Pointer to new stream
+ * @param[out] stream  Pointer to new stream
  * @param[in] cuMaskSize  Size of CU mask bit array passed in.
  * @param[in] cuMask Bit-vector representing the CU mask. Each active bit represents using one CU.
  * The first 32 bits represent the first 32 CUs, and so on. If its size is greater than physical
@@ -3275,7 +3275,7 @@ hipError_t hipStreamBatchMemOp(hipStream_t stream, unsigned int count,
 /**
  * @brief Creates a batch memory operation node and adds it to a graph.[BETA]
  *
- * @param [in] phGraphNode      - Returns the newly created node
+ * @param [out] phGraphNode     - Returns the newly created node
  * @param [in] hGraph           - Graph to which to add the node
  * @param [in] dependencies     -  Dependencies of the node
  * @param [in] numDependencies  - Number of dependencies
@@ -3372,7 +3372,7 @@ hipError_t hipGraphExecBatchMemOpNodeSetParams(hipGraphExec_t hGraphExec, hipGra
 /**
  * @brief Create an event with the specified flags
  *
- * @param[in,out] event Returns the newly created event.
+ * @param[out] event    Returns the newly created event.
  * @param[in] flags     Flags to control event behavior.  Valid values are #hipEventDefault,
  #hipEventBlockingSync, #hipEventDisableTiming, #hipEventInterprocess
  * #hipEventDefault : Default flag.  The event will use active synchronization and will support
@@ -3398,7 +3398,7 @@ hipError_t hipEventCreateWithFlags(hipEvent_t* event, unsigned flags);
 /**
  *  Create an event
  *
- * @param[in,out] event Returns the newly created event.
+ * @param[out] event Returns the newly created event.
  *
  * @returns #hipSuccess, #hipErrorNotInitialized, #hipErrorInvalidValue,
  * #hipErrorLaunchFailure, #hipErrorOutOfMemory
@@ -5543,7 +5543,7 @@ hipError_t hipArray3DGetDescriptor(HIP_ARRAY3D_DESCRIPTOR* pArrayDescriptor, hip
  *  @warning  Calling hipMemcpy2D with dst and src pointers that do not match the hipMemcpyKind
  * results in undefined behavior.
  *
- *  @param[in]   dst    Destination memory address
+ *  @param[out]  dst Destination memory address
  *  @param[in]   dpitch Pitch size in bytes of destination memory
  *  @param[in]   src    Source memory address
  *  @param[in]   spitch Pitch size in bytes of source memory
@@ -5605,7 +5605,7 @@ hipError_t hipMemcpyParam2DAsync(const hip_Memcpy2D* pCopy, hipStream_t stream _
  *  @note If host or dst are not pinned, the memory copy will be performed synchronously.  For
  * best performance, use hipHostMalloc to allocate host memory that is transferred asynchronously.
  *
- *  @param[in]   dst    Pointer to destination memory address
+ *  @param[out]  dst Pointer to destination memory address
  *  @param[in]   dpitch Pitch size in bytes of destination memory
  *  @param[in]   src    Pointer to source memory address
  *  @param[in]   spitch Pitch size in bytes of source memory
@@ -5624,7 +5624,7 @@ hipError_t hipMemcpy2DAsync(void* dst, size_t dpitch, const void* src, size_t sp
 /**
  *  @brief Copies data between host and device.
  *
- *  @param[in]   dst     Destination memory address
+ *  @param[out]  dst   Destination memory address
  *  @param[in]   wOffset Destination starting X offset
  *  @param[in]   hOffset Destination starting Y offset
  *  @param[in]   src     Source memory address
@@ -5643,7 +5643,7 @@ hipError_t hipMemcpy2DToArray(hipArray_t dst, size_t wOffset, size_t hOffset, co
 /**
  *  @brief Copies data between host and device.
  *
- *  @param[in]   dst     Destination memory address
+ *  @param[out]  dst Destination memory address
  *  @param[in]   wOffset Destination starting X offset
  *  @param[in]   hOffset Destination starting Y offset
  *  @param[in]   src     Source memory address
@@ -5664,7 +5664,7 @@ hipError_t hipMemcpy2DToArrayAsync(hipArray_t dst, size_t wOffset, size_t hOffse
 /**
  *  @brief Copies data between host and device.
  *
- *  @param[in]   dst Destination memory address
+ *  @param[out]  dst Destination memory address
  *  @param[in]   wOffsetDst Destination starting X offset
  *  @param[in]   hOffsetDst Destination starting Y offset
  *  @param[in]   src  Source memory address
@@ -5687,7 +5687,7 @@ hipError_t hipMemcpy2DArrayToArray(hipArray_t dst, size_t wOffsetDst, size_t hOf
  *
  *  @ingroup MemoryD
  *
- *  @param[in]   dst     Destination memory address
+ *  @param[out]  dst     Destination memory address
  *  @param[in]   wOffset Destination starting X offset
  *  @param[in]   hOffset Destination starting Y offset
  *  @param[in]   src     Source memory address
@@ -5708,7 +5708,7 @@ hipError_t hipMemcpyToArray(hipArray_t dst, size_t wOffset, size_t hOffset, cons
  *
  *  @ingroup MemoryD
  *
- *  @param[in]   dst       Destination memory address
+ *  @param[out]  dst       Destination memory address
  *  @param[in]   srcArray  Source memory address
  *  @param[in]   wOffset   Source starting X offset
  *  @param[in]   hOffset   Source starting Y offset
@@ -5727,7 +5727,7 @@ hipError_t hipMemcpyFromArray(void* dst, hipArray_const_t srcArray, size_t wOffs
 /**
  *  @brief Copies data between host and device.
  *
- *  @param[in]   dst       Destination memory address
+ *  @param[out]  dst       Destination memory address
  *  @param[in]   dpitch    Pitch of destination memory
  *  @param[in]   src       Source memory address
  *  @param[in]   wOffset   Source starting X offset
@@ -5746,7 +5746,7 @@ hipError_t hipMemcpy2DFromArray(void* dst, size_t dpitch, hipArray_const_t src, 
 /**
  *  @brief Copies data between host and device asynchronously.
  *
- *  @param[in]   dst       Destination memory address
+ *  @param[out]  dst       Destination memory address
  *  @param[in]   dpitch    Pitch of destination memory
  *  @param[in]   src       Source memory address
  *  @param[in]   wOffset   Source starting X offset
@@ -5767,7 +5767,7 @@ hipError_t hipMemcpy2DFromArrayAsync(void* dst, size_t dpitch, hipArray_const_t 
 /**
  *  @brief Copies data between host and device.
  *
- *  @param[in]   dst       Destination memory address
+ *  @param[out]  dst       Destination memory address
  *  @param[in]   srcArray  Source array
  *  @param[in]   srcOffset Offset in bytes of source array
  *  @param[in]   count     Size of memory copy in bytes
@@ -5781,7 +5781,7 @@ hipError_t hipMemcpyAtoH(void* dst, hipArray_t srcArray, size_t srcOffset, size_
 /**
  *  @brief Copies data between host and device.
  *
- *  @param[in]   dstArray   Destination memory address
+ *  @param[out]  dstArray Destination memory address
  *  @param[in]   dstOffset  Offset in bytes of destination array
  *  @param[in]   srcHost    Source host pointer
  *  @param[in]   count      Size of memory copy in bytes
@@ -5855,7 +5855,7 @@ hipError_t hipMemGetAddressRange(hipDeviceptr_t* pbase, size_t* psize, hipDevice
 /**
  * @brief Perform Batch of 1D copies
  *
- * @param [in] dsts      - Array of destination pointers
+ * @param [out] dsts  - Array of destination pointers
  * @param [in] srcs      - Array of source pointers.
  * @param [in] sizes     - Array of sizes for memcpy operations
  * @param [in] count     - Size of dsts, srcs and sizes arrays
